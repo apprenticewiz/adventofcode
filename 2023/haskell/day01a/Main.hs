319f7dee543cd3d2a712b83f08c6593fc265dbdd
Chris Parrott <chris@parrotts.net>
2024-01-11 14:05:09 -0500

    reworked solutions to be more concise

--- 2023/haskell/day01a/Main.hs
+++ 2023/haskell/day01a/Main.hs
@@ -1,51 +1,34 @@
 module Main ( main ) where
 
+import Data.Char
 import Data.List
 import Data.Maybe
 import System.Environment
 import System.Exit
 
-usage =
-    do
-        progName <- getProgName    
-        putStrLn ("usage: " ++ progName ++ " <file>")
-        exitFailure
+usage :: IO ()
+usage = do
+  progname <- getProgName
+  putStrLn $ "usage: " ++ progname ++ " <file>"
+  exitFailure
 
-findNumber [] line (minIndex, minDigit) (maxIndex, maxDigit) =
-    read ((fromJust minDigit) ++ (fromJust maxDigit)) :: Int
-findNumber (digit:digits) line (minIndex, minDigit) (maxIndex, maxDigit) =
-    let indices = findIndices (isPrefixOf digit) (tails line)
-        firstIndex = if not (null indices)
-                         then Just (head indices)
-                         else Nothing
-        lastIndex = if not (null indices)
-                        then Just (last indices)
-                        else Nothing
-        minPair = if (isJust firstIndex) && (fromJust firstIndex) <= minIndex
-                      then (fromJust firstIndex, Just digit)
-                      else (minIndex, minDigit)
-        maxPair = if (isJust lastIndex) && (fromJust lastIndex) >= maxIndex
-                      then (fromJust lastIndex, Just digit)
-                      else (maxIndex, maxDigit)
-    in findNumber digits line minPair maxPair
+process :: String -> Int
+process contents = sum $ map extractDigits $ lines contents
+  where
+    digits = "0123456789"
+    extractDigits :: String -> Int
+    extractDigits line = ((digitToInt leftDigit) * 10) + (digitToInt rightDigit)
+      where
+        occurrences = filter (`elem` digits) line
+        leftDigit = head occurrences
+        rightDigit = last occurrences
 
-processLines [] nums = nums
-processLines (line:lines) nums =
-    let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        number = findNumber digits line ((length line), Nothing) (0, Nothing)
-    in processLines lines (nums ++ [number])
-
-process contents =
-    let nums = processLines (lines contents) [] 
-    in sum nums
-
-main =
-    do
-        args <- getArgs
-        if (length args) < 1
-            then usage
-            else do
-                     let filename = head args
-                     contents <- readFile filename
-                     let result = process contents
-                     putStrLn ("result = " ++ (show result))
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      contents <- readFile filename
+      let result = process contents
+      putStrLn $ "result = " ++ show result
+    _ -> usage
--- 2023/haskell/day01b/Main.hs
+++ 2023/haskell/day01b/Main.hs
@@ -1,70 +1,59 @@
 module Main ( main ) where
 
 import Data.Char
+import qualified Data.Map.Strict as Map
 import Data.List
 import Data.Maybe
 import System.Environment
 import System.Exit
 
-usage =
-    do
-        progName <- getProgName    
-        putStrLn ("usage: " ++ progName ++ " <file>")
-        exitFailure
+usage :: IO ()
+usage = do
+  progname <- getProgName
+  putStrLn $ "usage: " ++ progname ++ " <file>"
+  exitFailure
 
-getDigit digit =
-    if all isDigit digit
-        then digit
-        else case digit of
-            "zero" -> "0"
-            "one" -> "1"
-            "two" -> "2"
-            "three" -> "3"
-            "four" -> "4"
-            "five" -> "5"
-            "six" -> "6"
-            "seven" -> "7"
-            "eight" -> "8"
-            "nine" -> "9"
-            _ -> error ("unexpected digit: " ++ (show digit))
+process :: String -> Int
+process contents = 
+  let contentLines = lines contents
+  in sum $ map processLine contentLines
 
-findNumber [] line (minIndex, minDigit) (maxIndex, maxDigit) =
-    read ((fromJust minDigit) ++ (fromJust maxDigit)) :: Int
-findNumber (digit:digits) line (minIndex, minDigit) (maxIndex, maxDigit) =
-    let indices = findIndices (isPrefixOf digit) (tails line)
-        firstIndex = if not (null indices)
-                         then Just (head indices)
-                         else Nothing
-        lastIndex = if not (null indices)
-                        then Just (last indices)
-                        else Nothing
-        minPair = if (isJust firstIndex) && (fromJust firstIndex) <= minIndex
-                      then (fromJust firstIndex, Just (getDigit digit))
-                      else (minIndex, minDigit)
-        maxPair = if (isJust lastIndex) && (fromJust lastIndex) >= maxIndex
-                      then (fromJust lastIndex, Just (getDigit digit))
-                      else (maxIndex, maxDigit)
-    in findNumber digits line minPair maxPair
+processLine :: String -> Int
+processLine line =
+  let digitsMap = Map.fromList [
+        ("0", 0), ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6),
+        ("7", 7), ("8", 8), ("9", 9), ("zero", 0), ("one", 1), ("two", 2),
+        ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7),
+        ("eight", 8), ("nine", 9) ]
+      findString needle haystack = findIndices (isPrefixOf needle) (tails haystack)
+      (minPair, maxPair) = foldl (\pair digit ->
+          let occurrences = findString digit line
+          in case pair of
+            (Nothing, Nothing) ->
+              if (null occurrences)
+                then (Nothing, Nothing)
+                else (Just (digit, (head occurrences)), Just (digit, (last occurrences)))
+            (Just (minDigit, minIndex), Just (maxDigit, maxIndex)) ->
+              if (null occurrences)
+                then (Just (minDigit, minIndex), Just (maxDigit, maxIndex))
+                else (Just (if (head occurrences) < minIndex
+                              then (digit, (head occurrences))
+                              else (minDigit, minIndex)),
+                      Just (if (last occurrences) > maxIndex
+                              then (digit, (last occurrences))
+                              else (maxDigit, maxIndex)))
+            _ -> undefined
+        ) (Nothing, Nothing) (Map.keys digitsMap)
+      leftDigit = fromJust $ Map.lookup (fst $ fromJust minPair) digitsMap
+      rightDigit = fromJust $ Map.lookup (fst $ fromJust maxPair) digitsMap
+  in (leftDigit * 10) + rightDigit
 
-processLines [] nums = nums
-processLines (line:lines) nums =
-    let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                  "zero", "one", "two", "three", "four", "five", "six", 
-                  "seven", "eight", "nine"]
-        number = findNumber digits line ((length line), Nothing) (0, Nothing)
-    in processLines lines (nums ++ [number])
-
-process contents =
-    let nums = processLines (lines contents) [] 
-    in sum nums
-
-main =
-    do
-        args <- getArgs
-        if (length args) < 1
-            then usage
-            else do
-                     let filename = head args
-                     contents <- readFile filename
-                     let result = process contents
-                     putStrLn ("result = " ++ (show result))
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      contents <- readFile filename
+      let result = process contents
+      putStrLn $ "result = " ++ show result
+    _ -> usage
