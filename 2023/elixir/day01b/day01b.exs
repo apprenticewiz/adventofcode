1eeb93f629d4d5898f1dc1848e4778a78c7328bb
Chris Parrott <chris@parrotts.net>
2024-01-16 01:32:11 -0500

    formatting

--- 2023/elixir/day01b/day01b.exs
+++ 2023/elixir/day01b/day01b.exs
@@ -8,7 +8,8 @@ defmodule Day01b do
 
   def process(contents) do
     lines = String.split(contents, "\n", trim: true)
-    digits_map = %{"0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4, "5" => 5,
+    digits_map = %{
+      "0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4, "5" => 5,
       "6" => 6 , "7" => 7, "8" => 8, "9" => 9, "zero" => 0, "one" => 1,
       "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6,
       "seven" => 7, "eight" => 8, "nine" => 9
