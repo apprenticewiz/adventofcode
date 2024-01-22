9afc42dcfb11f08e5964e8129ce7e472eca6db83
Chris Parrott <chris@parrotts.net>
2024-01-22 13:08:26 -0500

    improved day03b solution, rewritten to use functional programming techniques

--- 2023/clojure/day03b/src/day03b/core.clj
+++ 2023/clojure/day03b/src/day03b/core.clj
@@ -9,64 +9,69 @@
 (defrecord Position [row col])
 
 (defn build-numbers [contents]
-  (let [number-str (atom "")
-        scanning-number (atom false)
-        current-loc (atom nil)
-        number-locs (atom {})]
-    (doseq [[row line] (map-indexed vector (str/split-lines contents))]
-      (doseq [[col ch] (map-indexed vector line)]
-        (if @scanning-number
-          (if (Character/isDigit ch)
-            (swap! number-str str ch)
-            (do
-              (reset! number-locs (assoc @number-locs @current-loc @number-str))
-              (reset! current-loc nil)
-              (reset! number-str "")
-              (reset! scanning-number false)))
-          (do
-            (if (Character/isDigit ch)
-              (do
-                (reset! scanning-number true)
-                (swap! number-str str ch)
-                (reset! current-loc (->Position row col)))))))
-      (if @scanning-number
-        (do
-          (reset! number-locs (assoc @number-locs @current-loc @number-str))
-          (reset! current-loc nil)
-          (reset! number-str "")
-          (reset! scanning-number false))))
-    @number-locs))
+  (nth (reduce
+    (fn [[row num-locs] line]
+      [(+ row 1) 
+      (nth (reduce
+          (fn [[col scanning-number number current-pos num-locs] ch]
+            (if scanning-number
+              (if (Character/isDigit ch)
+                (if (= (+ col 1) (count line))
+                  [(+ col 1) false "" (->Position -1 -1) (assoc num-locs current-pos (str number ch))]
+                  [(+ col 1) true (str number ch) current-pos num-locs])
+                [(+ col 1) false "" (->Position -1 -1) (assoc num-locs current-pos number)])
+              (if (Character/isDigit ch)
+                [(+ col 1) true (str ch) (->Position row col) num-locs]
+                [(+ col 1) false "" (->Position -1 -1) num-locs])))
+          [0 false "" (->Position -1 -1) num-locs]
+          line) 4)])
+    [0 {}]
+    (str/split-lines contents)) 1))
 
 (defn build-gears [contents]
-  (let [gear-locs (atom {})]
-    (doseq [[row line] (map-indexed vector (str/split-lines contents))]
-      (doseq [[col ch] (map-indexed vector line)]
-        (if (= ch \*)
-          (reset! gear-locs (assoc @gear-locs (->Position row col) ch)))))
-    @gear-locs))
+  (nth (reduce
+    (fn [[row part-locs] line]
+      [(+ row 1) (nth (reduce
+        (fn [[col part-locs] ch]
+            (if (= ch \*)
+              [(+ col 1) (assoc part-locs (->Position row col) ch)]
+              [(+ col 1) part-locs]))
+        [0 part-locs]
+        line) 1)])
+    [0 {}]
+    (str/split-lines contents)) 1))
 
 (defn check-gears [number-locs gear-locs]
-  (let [result (atom 0)]
-    (doseq [gear-loc (keys gear-locs)]
-      (let [adjacents (atom [])]
-        (doseq [number-loc (keys number-locs)]
-          (let [skip (atom false)]
-            (doseq [delta-row (range -1 2)]
-              (let [adjacent-row (+ (-> gear-loc :row) delta-row)]
-                (doseq [delta-col (range -1 2)]
-                  (let [adjacent-col (+ (-> gear-loc :col) delta-col)
-                        number-col-start (-> number-loc :col)
-                        number-col-end (+ (-> number-loc :col) (count (get number-locs number-loc)))]
-                    (doseq [number-col (range number-col-start number-col-end)]
-                      (if (and (not @skip)
-                               (= adjacent-row (-> number-loc :row))
-                               (= adjacent-col number-col))
-                        (do
-                          (reset! skip true)
-                          (swap! adjacents conj (Integer/parseInt (get number-locs number-loc))))))))))))
-        (if (= (count @adjacents) 2)
-          (swap! result + (reduce * @adjacents)))))
-    @result))
+  (reduce
+    (fn [result gear-loc]
+      (let [adjacents (reduce
+          (fn [adjacents-vec number-loc]
+            (let [number-row (-> number-loc :row)
+                  number-col-first (-> number-loc :col)
+                  number-col-last (+ (-> number-loc :col) (count (get number-locs number-loc)))
+                  found-adjacent (reduce
+                    (fn [found neighbor]
+                      (let [adjacent-pos (->Position (+ (-> gear-loc :row) (-> neighbor :row))
+                                                     (+ (-> gear-loc :col) (-> neighbor :col)))]
+                        (if (and (= (-> adjacent-pos :row) number-row)
+                                 (>= (-> adjacent-pos :col) number-col-first)
+                                 (< (-> adjacent-pos :col) number-col-last))
+                          true
+                          found)))
+                    false
+                    [(->Position -1 -1) (->Position -1 0) (->Position -1 1)
+                     (->Position 0 -1) (->Position 0 1)
+                     (->Position 1 -1) (->Position 1 0) (->Position 1 1)])]
+              (if found-adjacent
+                (conj adjacents-vec (get number-locs number-loc))
+                adjacents-vec)))
+          []
+          (keys number-locs))]
+        (if (= (count adjacents) 2)
+          (+ result (reduce * 1 (map (fn [n] (Integer/parseInt n)) adjacents)))
+          result)))
+    0
+    (keys gear-locs)))
 
 (defn process [contents]
   (let [number-locs (build-numbers contents)
