0928eccf115c4abbc5553633ac3fb94d29f7b8e1
Chris Parrott <chris@parrotts.net>
2024-01-29 02:19:30 -0500

    2023 day 3 solution in Swift

---
+++ 2023/swift/day03a/Sources/main.swift
@@ -0,0 +1,116 @@
+import Foundation
+
+func usage() {
+    let progname = (CommandLine.arguments[0] as NSString).lastPathComponent
+    print("usage: swift run \(progname) <file>")
+    exit(1)
+}
+
+func buildNumbers(contents: String) -> [(Int, Int, String)] {
+    var numLocs: [(Int, Int, String)] = []
+    var row = 0
+    var scanningNumber = false
+    var number = ""
+    var currentPos = (-1, -1)
+    for line in contents.split(separator: "\n") {
+        var col = 0
+        for ch in line {
+            if scanningNumber {
+                if ch.isNumber {
+                    number += String(ch)
+                } else {
+                    numLocs.append((currentPos.0, currentPos.1, number))
+                    number = ""
+                    scanningNumber = false
+                }
+            } else {
+                if ch.isNumber {
+                    currentPos = (row, col)
+                    number += String(ch)
+                    scanningNumber = true
+                }
+            }
+            col += 1
+        }
+        if scanningNumber {
+            numLocs.append((currentPos.0, currentPos.1, number))
+            number = ""
+            scanningNumber = false
+        }
+        row += 1
+    }
+    return numLocs
+}
+
+func buildParts(contents: String) -> [(Int, Int, Character)] {
+    var partLocs: [(Int, Int, Character)] = []
+    var row = 0
+    for line in contents.split(separator: "\n") {
+        var col = 0
+        for ch in line {
+            if !ch.isNumber && ch != "." {
+                partLocs.append((row, col, ch))
+            }
+            col += 1
+        }
+        row += 1
+    }
+    return partLocs
+}
+
+func checkParts(numLocs: [(Int, Int, String)], partLocs: [(Int, Int, Character)]) -> Int {
+    var result = 0
+    let neighbors = [ (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for numLoc in numLocs {
+        let numRow = numLoc.0
+        let numColFirst = numLoc.1
+        let number = numLoc.2
+        let numColLast = numColFirst + number.count - 1
+        var found = false
+        for numCol in numColFirst...numColLast {
+            for neighbor in neighbors {
+                let adjPos = (numRow + neighbor.0, numCol + neighbor.1)
+                for partLoc in partLocs {
+                    let partRow = partLoc.0
+                    let partCol = partLoc.1
+                    if (partRow == adjPos.0) && (partCol == adjPos.1) {
+                        found = true
+                        break
+                    }
+                }
+                if found {
+                    break
+                }
+            }
+            if found {
+                break
+            }
+        }
+        if found {
+            result += Int(number)!
+        }
+    }
+    return result
+}
+
+func process(contents: String) -> Int {
+    let numLocs = buildNumbers(contents: contents)
+    let partLocs = buildParts(contents: contents)
+    return checkParts(numLocs: numLocs, partLocs: partLocs)
+}
+
+func main() {
+    if CommandLine.arguments.count < 2 {
+        usage()
+    }
+    let filename = CommandLine.arguments[1]
+    guard let contents = try? String(contentsOfFile: filename, encoding: .utf8) else {
+        let message = "error: could not read contents of file \(filename).\n"
+        FileHandle.standardError.write(message.data(using: .utf8)!)
+        exit(1)
+    }
+    let result = process(contents: contents)
+    print("result = \(result)")
+}
+
+main()
