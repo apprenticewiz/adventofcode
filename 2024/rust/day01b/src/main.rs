1cb20017f298c4ca95bcc21f23f886e187e0312b
Chris Parrott <chris@parrotts.net>
2024-12-02 22:18:49 -0500

    2024 day 1 solution in Rust

---
+++ 2024/rust/day01b/src/main.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn usage() {
+    let progname = env::args().next().unwrap();
+    println!("usage: {progname} <file>");
+    process::exit(1);
+}
+
+fn build_lists(contents: &str) -> (Vec<u32>, Vec<u32>) {
+    let mut first_list = vec![];
+    let mut second_list = vec![];
+    for line in contents.lines() {
+        let first_number = line.split_whitespace().next().unwrap().parse::<u32>().unwrap();
+        let second_number = line.split_whitespace().nth(1).unwrap().parse::<u32>().unwrap();
+        first_list.push(first_number);
+        second_list.push(second_number);
+    }
+    first_list.sort();
+    second_list.sort();
+    (first_list, second_list)
+}
+
+fn process(contents: &str) -> u32 {
+    let mut result = 0;
+    let (first_list, second_list) = build_lists(contents);
+    for first in first_list.iter() {
+        result += *first * second_list.iter().filter(|&n| *n == *first).count() as u32;
+    }
+    result
+}
+
+fn main() {
+    if env::args().count() < 2 {
+        usage();
+    }
+    let filename = env::args().nth(1).unwrap();
+    let contents = fs::read_to_string(filename).expect("read of input file failed");
+    let result = process(&contents);
+    println!("result = {result}");
+}
