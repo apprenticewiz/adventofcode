b6a330143fdbcf266d4b937cc78815fd966a524e
Chris Parrott <chris@parrotts.net>
2024-12-05 02:11:58 -0500

    2024 day 5 solution in Haskell

---
+++ 2024/haskell/day05a/Main.hs
@@ -0,0 +1,71 @@
+module Main ( main ) where
+
+import Data.IntMap ( IntMap )
+import qualified Data.IntMap as IntMap
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure )
+
+import Debug.Trace
+
+usage :: IO ()
+usage = do
+  progname <- getProgName
+  putStrLn $ "usage: " ++ progname ++ " <file>"
+  exitFailure
+
+splitOn :: Char -> String -> [String]
+splitOn ch s =
+    case dropWhile (== ch) s of
+        "" -> []
+        s' -> w : splitOn ch s''
+            where (w, s'') = break (== ch) s'
+
+makeRulesTable :: [String] -> IntMap [Int]
+makeRulesTable =
+    foldl
+        (\currentMap ruleTxt ->
+            let (first, second) = (takeWhile (/= '|') ruleTxt, tail (dropWhile (/= '|') ruleTxt))
+                firstNum = read first
+                secondNum = read second
+            in if IntMap.notMember firstNum currentMap
+                then IntMap.insert firstNum [ secondNum ] currentMap
+                else
+                    let items = currentMap IntMap.! firstNum
+                    in IntMap.insert firstNum (items ++ [secondNum]) currentMap
+        )
+        IntMap.empty
+
+processUpdates :: IntMap [Int] -> [[Int]] -> Int
+processUpdates rulesTable updates =
+    let isValid [x] = True
+        isValid (x:xs) = all (\y -> IntMap.member x rulesTable && (y `elem` (rulesTable IntMap.! x))) xs && isValid xs
+    in
+        foldl
+            (\acc update ->
+                if isValid update
+                    then
+                        let middleIndex = length update `div` 2
+                            middleElem = update !! middleIndex
+                        in acc + middleElem
+                    else acc
+            )
+            0
+            updates
+
+process :: String -> Int
+process contents =
+    let splitInput inputLines = (takeWhile (/= "") inputLines, tail (dropWhile (/= "") inputLines))
+        (rules, updates) = splitInput (lines contents)
+        rulesTable = makeRulesTable rules
+        updatesList = map (map read . splitOn ',') updates
+    in processUpdates rulesTable updatesList
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      contents <- readFile filename
+      let result = process contents
+      putStrLn $ "result = " ++ show result
+    _ -> usage
---
+++ 2024/haskell/day05b/Main.hs
@@ -0,0 +1,76 @@
+module Main ( main ) where
+
+import Data.IntMap ( IntMap )
+import qualified Data.IntMap as IntMap
+import Data.List ( sortBy )
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure )
+
+usage :: IO ()
+usage = do
+  progname <- getProgName
+  putStrLn $ "usage: " ++ progname ++ " <file>"
+  exitFailure
+
+splitOn :: Char -> String -> [String]
+splitOn ch s =
+    case dropWhile (== ch) s of
+        "" -> []
+        s' -> w : splitOn ch s''
+            where (w, s'') = break (== ch) s'
+
+makeRulesTable :: [String] -> IntMap [Int]
+makeRulesTable =
+    foldl
+        (\currentMap ruleTxt ->
+            let (first, second) = (takeWhile (/= '|') ruleTxt, tail (dropWhile (/= '|') ruleTxt))
+                firstNum = read first
+                secondNum = read second
+            in if IntMap.notMember firstNum currentMap
+                then IntMap.insert firstNum [ secondNum ] currentMap
+                else
+                    let items = currentMap IntMap.! firstNum
+                    in IntMap.insert firstNum (items ++ [secondNum]) currentMap
+        )
+        IntMap.empty
+
+processUpdates :: IntMap [Int] -> [[Int]] -> Int
+processUpdates rulesTable updates =
+    let isValid [x] = True
+        isValid (x:xs) = all (\y -> IntMap.member x rulesTable && (y `elem` (rulesTable IntMap.! x))) xs && isValid xs
+        comparePages x y
+          | x == y = EQ
+          | IntMap.member x rulesTable && (y `elem` (rulesTable IntMap.! x)) = LT
+          | otherwise = GT
+        fixup = sortBy comparePages
+    in
+        foldl
+            (\acc update ->
+                if isValid update
+                    then acc
+                    else
+                        let fixedUpdate = fixup update
+                            middleIndex = length fixedUpdate `div` 2
+                            middleElem = fixedUpdate !! middleIndex
+                        in acc + middleElem
+            )
+            0
+            updates
+
+process :: String -> Int
+process contents =
+    let splitInput inputLines = (takeWhile (/= "") inputLines, tail (dropWhile (/= "") inputLines))
+        (rules, updates) = splitInput (lines contents)
+        rulesTable = makeRulesTable rules
+        updatesList = map (map read . splitOn ',') updates
+    in processUpdates rulesTable updatesList
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      contents <- readFile filename
+      let result = process contents
+      putStrLn $ "result = " ++ show result
+    _ -> usage
