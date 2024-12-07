2f49b641e5da6e3854eece72d04e5aa80aada26b
Chris Parrott <chris@parrotts.net>
2024-12-07 16:25:59 -0500

    2024 day 7 solution in Haskell

---
+++ 2024/haskell/day07b/Main.hs
@@ -0,0 +1,50 @@
+module Main ( main ) where
+
+import qualified Data.Text as Text
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure )
+
+import Debug.Trace ( trace )
+
+usage :: IO ()
+usage = do
+  progname <- getProgName
+  putStrLn $ "usage: " ++ progname ++ " <file>"
+  exitFailure
+
+process :: String -> Int
+process contents =
+    let splitLine line = ((read $ takeWhile (/= ':') line) :: Int, (words . tail) $ dropWhile (/= ':') line)
+        generateEquations [x] = [show x]
+        generateEquations (x:xs) = [show x ++ " " ++ op ++ " " ++ eqns | op <- ["+", "*", "||"], eqns <- generateEquations xs]
+        solve [] = []
+        solve [x] = x
+        solve (x:op:y:zs) =
+            case op of
+                "+" -> solve (show (read x + read y):zs)
+                "*" -> solve (show (read x * read y):zs)
+                "||" -> solve ((x ++ y):zs)
+                _ -> error "unexpected op"
+        validSolutions =
+            foldl
+                (\valid line ->
+                    let (lhs, rhs) = splitLine line
+                        numberList = map read rhs :: [Int]
+                        solutions = map (read . solve . words) (generateEquations numberList)
+                    in if lhs `elem` solutions
+                        then valid ++ [lhs]
+                        else valid
+                )
+                []
+                (lines contents)
+    in sum validSolutions
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      contents <- readFile filename
+      let result = process contents
+      putStrLn $ "result = " ++ show result
+    _ -> usage
