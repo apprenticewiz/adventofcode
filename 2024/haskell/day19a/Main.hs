8fe24568b152db8dc0e6fb6209ebadb5e1e59844
Chris Parrott <chris@parrotts.net>
2024-12-21 17:40:51 -0500

    2024 day 19 solution in Haskell

---
+++ 2024/haskell/day19a/Main.hs
@@ -0,0 +1,65 @@
+module Main ( main ) where
+
+import Data.List ( nub )
+import Data.Maybe ( mapMaybe )
+import Data.Text (Text)
+import qualified Data.Text as Text
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure )
+
+type Position = (Int, Int)
+
+usage :: IO ()
+usage = do
+    progname <- getProgName
+    putStrLn $ "usage: " ++ progname ++ " <file>"
+    exitFailure
+
+parseInput :: String -> ([Text], [Text])
+parseInput contents =
+    let contentLines = lines contents
+        towels = Text.splitOn (Text.pack ", ") $ Text.pack $ head contentLines
+        patterns = map Text.pack $ drop 2 contentLines
+    in (towels, patterns)
+
+reduceTowels :: [Text] -> [Text]
+reduceTowels towels =
+    let comboTowels s towels = any (\(x, y) -> x <> y == s) pairs
+            where
+                pairs = [(x, y) | x <- towels, y <- towels, x /= y]
+        removeComboTowels towels = filter (`notElem` toRemove) towels
+            where
+                toRemove = [x | x <- towels, comboTowels x towels]
+    in removeComboTowels towels
+
+isValid :: [Text] -> Text -> Bool
+isValid towels pattern = checkPattern [(Text.empty, pattern)]
+    where
+        checkPattern :: [(Text, Text)] -> Bool
+        checkPattern !q
+            | null q = False
+            | otherwise =
+                let (matched, toScan) = head q
+                    rest = tail q
+
+                    matches = filter (`Text.isPrefixOf` toScan) towels
+                    newEntries = map (\match -> (matched <> match, Text.drop (Text.length match) toScan)) matches
+                in (any (Text.null . snd) newEntries || checkPattern (nub (rest ++ newEntries)))
+
+countValid :: [Text] -> [Text] -> Int
+countValid towels = foldl (\count pattern -> if isValid towels pattern then count + 1 else count) 0
+
+process :: String -> Int
+process contents =
+    let (towels, patterns) = parseInput contents
+    in countValid (reduceTowels towels) patterns
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [filename] -> do
+            contents <- readFile filename
+            let result = process contents
+            putStrLn $ "result = " ++ show result
+        _ -> usage
---
+++ 2024/haskell/day19b/Main.hs
@@ -0,0 +1,70 @@
+module Main ( main ) where
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Text (Text)
+import qualified Data.Text as Text
+import System.Environment ( getArgs, getProgName )
+import System.Exit ( exitFailure )
+
+type Position = (Int, Int)
+
+usage :: IO ()
+usage = do
+    progname <- getProgName
+    putStrLn $ "usage: " ++ progname ++ " <file>"
+    exitFailure
+
+parseInput :: String -> ([Text], [Text])
+parseInput contents =
+    let contentLines = lines contents
+        towels = Text.splitOn (Text.pack ", ") $ Text.pack $ head contentLines
+        patterns = map Text.pack $ drop 2 contentLines
+    in (towels, patterns)
+
+updateMemo :: Map Text Int -> Text -> Int -> Map Text Int
+updateMemo memo design arrangements = Map.insertWith (+) design arrangements memo
+
+possibleArrangements :: [Text] -> Text -> Int -> Map Text Int -> (Int, Map Text Int)
+possibleArrangements towels design maxTowelLength memo
+    | Map.member design memo = (Map.findWithDefault 0 design memo, memo)
+    | Text.null design = (0, memo)
+    | otherwise = process (1, memo)
+                where
+                    process :: (Int, Map Text Int) -> (Int, Map Text Int)
+                    process (i, currentMemo)
+                        | i > min (Text.length design) maxTowelLength =
+                            if not (Map.member design currentMemo)
+                                then let currentMemo' = Map.insert design 0 currentMemo
+                                     in (currentMemo' Map.! design, currentMemo')
+                                else (currentMemo Map.! design, currentMemo)
+                        | otherwise =
+                            let current = Text.take i design
+                            in if current `elem` towels
+                                then if Text.length design == i
+                                    then process (i + 1, updateMemo currentMemo design 1)
+                                    else let (rhsArrangements, currentMemo') = possibleArrangements towels (Text.drop i design) maxTowelLength currentMemo
+                                         in process (i + 1, updateMemo currentMemo' design rhsArrangements)
+                                else process (i + 1, currentMemo)
+
+countPossible :: [Text] -> [Text] -> Int
+countPossible towels designs = 
+    let maxTowelLength = maximum (map Text.length towels)  -- Find the maximum towel length
+        memo = Map.empty
+        totalArrangements = sum $ map (\design -> fst $ possibleArrangements towels design maxTowelLength memo) designs
+    in totalArrangements
+
+process :: String -> Int
+process contents =
+    let (towels, patterns) = parseInput contents
+    in countPossible towels patterns
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [filename] -> do
+            contents <- readFile filename
+            let result = process contents
+            putStrLn $ "result = " ++ show result
+        _ -> usage
