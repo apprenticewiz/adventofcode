94c0808238715d27a8fd66b164eefc72bdf9aeda
Chris Parrott <cparrott@topaz.local>
2025-07-04 23:00:59 -0700

    2015 day 4 in C++

---
+++ c++/2015/day04a/day04a.cpp
@@ -0,0 +1,48 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "utils/md5.h"
+
+void usage(std::string progname)
+{
+    std::cerr << "usage: " << progname << " <key>" << std::endl;
+    std::exit(1);
+}
+
+uint32_t process(std::string key)
+{
+    uint32_t n = 1;
+    for ( ; ; ) {
+        MD5 md5;
+	md5.reset();
+        std::ostringstream key_builder;
+        key_builder << key << n;
+        std::string try_key = key_builder.str();
+        md5.update(try_key);
+        std::ostringstream output_builder;
+	std::string digest = md5.digest();
+        if ( digest.find("00000") == 0 ) {
+            break;
+        } else {
+            ++n;
+        }
+    }
+    return n;
+}
+
+int main(int argc, char* argv[])
+{
+    std::string progname(argv[0]);
+    if ( argc < 2 ) {
+        usage(progname);
+    }
+
+    std::string key(argv[1]);
+    uint32_t result = process(key);
+    std::cout << "result = " << result << std::endl;
+    return 0;
+}
