30f1886c4ffc2b5d8155037efa42cad14ec9753a
Chris Parrott <cparrott@topaz.local>
2025-07-04 23:02:32 -0700

    2015 day 1 in Rust

---
+++ rust/2015/day01a/src/main.rs
@@ -0,0 +1,43 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::process;
+
+fn usage(progname: &str) {
+    eprintln!("usage: {} <input file>", progname);
+    process::exit(1);
+}
+
+fn process_file(filename: &str) -> io::Result<i32> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let mut counter: i32 = 0;
+    for line_result in reader.lines() {
+        let line = line_result?;
+        for ch in line.chars() {
+            match ch {
+                '(' => counter += 1,
+                ')' => counter -= 1,
+                _ => {}
+            }
+        }
+    }
+
+    Ok(counter)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let progname = &args[0];
+    if args.len() < 2 {
+        usage(progname);
+    }
+    let filename = &args[1];
+    match process_file(filename) {
+        Ok(result) => println!("result = {}", result),
+        Err(e) => {
+            eprintln!("error while processing file `{}': {}", filename, e);
+            process::exit(1);
+        }
+    }
+}
---
+++ rust/2015/day01b/src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Error, BufRead};
+use std::process;
+
+fn usage(progname: &str) {
+    eprintln!("usage: {} <input file>", progname);
+    process::exit(1);
+}
+
+fn process_file(filename: &str) -> io::Result<u32> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let mut counter: i32 = 0;
+    for line_result in reader.lines() {
+        let line = line_result?;
+        for (i, ch) in line.chars().enumerate() {
+            match ch {
+                '(' => counter += 1,
+                ')' => counter -= 1,
+                _ => {}
+            }
+            if counter < 0 {
+                return Ok(i as u32 + 1);
+            }
+        }
+    }
+    Err(Error::other("result was never negative"))
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let progname = &args[0];
+    if args.len() < 2 {
+        usage(progname);
+    }
+    let filename = &args[1];
+    match process_file(filename) {
+        Ok(result) => println!("result = {}", result),
+        Err(e) => {
+            eprintln!("error while processing file `{}': {}", filename, e);
+            process::exit(1);
+        }
+    }
+}
