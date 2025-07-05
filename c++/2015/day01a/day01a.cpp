164df7dc9fb2931cf2835ea1258b79edac15d67a
Chris Parrott <cparrott@topaz.local>
2025-07-04 22:58:58 -0700

    2015 day 1 in C++

---
+++ c++/2015/day01a/day01a.cpp
@@ -0,0 +1,44 @@
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void usage(std::string progname)
+{
+    std::cerr << "usage: " << progname << " <input file>" << std::endl;
+    std::exit(1);
+}
+
+int32_t process(std::string filename)
+{
+    int32_t counter = 0;
+    std::ifstream infile(filename);
+    std::string line;
+    while ( std::getline(infile, line) ) {
+        for ( auto ch = line.begin(); ch != line.end(); ch++ ) {
+            switch ( *ch ) {
+                case '(':
+                    ++counter;
+                    break;
+                case ')':
+                    --counter;
+                    break;
+            }
+        }
+    }
+    return counter;
+}
+
+int main(int argc, char* argv[])
+{
+    std::string progname(argv[0]);
+    if ( argc < 2 ) {
+        usage(progname);
+    }
+
+    std::string filename(argv[1]);
+    int32_t result = process(filename);
+    std::cout << "result = " << result << std::endl;
+    return 0;
+}
---
+++ c++/2015/day01b/day01b.cpp
@@ -0,0 +1,49 @@
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void usage(std::string progname)
+{
+    std::cerr << "usage: " << progname << " <input file>" << std::endl;
+    std::exit(1);
+}
+
+uint32_t process(std::string filename)
+{
+    int32_t counter = 0;
+    uint32_t position = 0;
+    std::ifstream infile(filename);
+    std::string line;
+    while ( std::getline(infile, line) ) {
+        for ( auto ch = line.begin(); ch != line.end(); ch++ ) {
+            ++position;
+            switch ( *ch ) {
+                case '(':
+                    ++counter;
+                    break;
+                case ')':
+                    --counter;
+                    break;
+            }
+            if ( counter < 0 ) {
+                break;
+            }
+        }
+    }
+    return position;
+}
+
+int main(int argc, char* argv[])
+{
+    std::string progname(argv[0]);
+    if ( argc < 2 ) {
+        usage(progname);
+    }
+
+    std::string filename(argv[1]);
+    uint32_t result = process(filename);
+    std::cout << "result = " << result << std::endl;
+    return 0;
+}
