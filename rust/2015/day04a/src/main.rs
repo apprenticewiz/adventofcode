31387a2577cec7d9b28b675791d62d8780546351
Chris Parrott <chris@parrotts.net>
2025-07-07 16:19:05 -0700

    2015 day 4 in Rust

---
+++ rust/2015/day04a/src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::io;
+use std::process;
+
+use aoc_utils::md5;
+
+fn usage(progname: &str) {
+    eprintln!("usage: {progname} <key>");
+    process::exit(1);
+}
+
+fn process_input(key: &str) -> io::Result<u32> {
+    let mut n: u32 = 1;
+    loop {
+        let mut try_key = String::from(key);
+        try_key.push_str(format!("{n}").as_str());
+        let digest = md5::md5(try_key.as_str());
+        if digest.as_str().starts_with("00000") {
+            return Ok(n);
+        }
+        n += 1;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let progname = &args[0];
+    if args.len() < 2 {
+        usage(progname);
+    }
+    let key = &args[1];
+    match process_input(key) {
+        Ok(result) => println!("result = {result}"),
+        Err(e) => {
+            eprintln!("error while processing input: {e}");
+            process::exit(1);
+        }
+    }
+}
