d0a5657d898dbaf2d0fec5cbae06981a14e6ba31
Chris Parrott <chris@parrotts.net>
2025-07-08 15:17:30 -0700

    2015 day 2 in Java

---
+++ java/2015/day02b/Program.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+public class Program {
+	public static void usage() {
+		System.err.println("usage: java -jar Program.jar <input file>");
+		System.exit(1);
+	}
+
+	public static int process(String filename) {
+		int totalLen = 0;
+		try ( BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ( (line = reader.readLine()) != null ) {
+				String[] numStrs = line.split("x");
+				Vector<Integer> dimensions = new Vector<Integer>();
+				for ( String numStr : numStrs ) {
+					dimensions.addElement(Integer.parseInt(numStr));
+				}
+				int perim1 = (dimensions.elementAt(0) + dimensions.elementAt(1)) * 2;
+				int perim2 = (dimensions.elementAt(0) + dimensions.elementAt(2)) * 2;
+				int perim3 = (dimensions.elementAt(1) + dimensions.elementAt(2)) * 2;
+				int presentLen = Integer.min(Integer.min(perim1, perim2), perim3);
+				int bowLen = dimensions.elementAt(0) * dimensions.elementAt(1) * dimensions.elementAt(2);
+				totalLen += presentLen + bowLen;
+			}
+		} catch ( IOException e ) {
+			System.err.println("Error reading file: " + e.getMessage());
+			System.exit(1);
+		}
+		return totalLen;
+	}
+
+	public static final void main(String[] args) {
+		if ( args.length < 1 ) {
+			usage();
+		}
+		String filename = args[0];
+		int result = process(filename);
+		System.out.println("result = " + result);
+	}
+}
