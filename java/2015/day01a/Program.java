c3a4129ddf4fddd19917b4c8881e519946c1c014
Chris Parrott <chris@parrotts.net>
2025-07-08 15:17:12 -0700

    2015 day 1 in Java

---
+++ java/2015/day01a/Program.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+	public static void usage() {
+		System.err.println("usage: java -jar Program.jar <input file>");
+		System.exit(1);
+	}
+
+	public static int process(String filename) {
+		int counter = 0;
+		try ( BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ( (line = reader.readLine()) != null ) {
+				for ( char ch : line.toCharArray() ) {
+					switch ( ch ) {
+						case '(':
+							counter++;
+							break;
+						case ')':
+							counter--;
+							break;
+					}
+				}
+			}
+		} catch ( IOException e ) {
+			System.err.println("Error reading file: " + e.getMessage());
+			System.exit(1);
+		}
+		return counter;
+	}
+
+	public static final void main(String[] args) {
+		if ( args.length < 1 ) {
+			usage();
+		}
+		String filename = args[0];
+		int result = process(filename);
+		System.out.println("result = " + result);
+	}
+}
