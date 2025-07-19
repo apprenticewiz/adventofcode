55ac00b9729ee7b7067bd213740a4daccee4395e
Chris Parrott <chris@parrotts.net>
2025-07-19 12:17:39 -0700

    add Go template

---
+++ go/templates/template.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func usage(progname string) {
+	fmt.Fprintf(os.Stderr, "usage: %s <input file>\n", progname)
+	os.Exit(1)
+}
+
+func process(filename string) int32 {
+	var result int32 = 0
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		/* do something here with each line */
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	return result
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		usage(os.Args[0])
+	}
+
+	filename := os.Args[1]
+	result := process(filename)
+	fmt.Printf("result = %d\n", result)
+}
