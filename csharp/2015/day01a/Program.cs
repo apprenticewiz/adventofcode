960a0086206bab52e181a72457b459665e6c8f33
Chris Parrott <chris@parrotts.net>
2025-07-19 14:18:02 -0700

    2015 day 1 in C#

---
+++ csharp/2015/day01a/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Usage()
+    {
+        Console.Error.WriteLine("usage: dotnet run <input file>");
+        Environment.Exit(1);
+    }
+
+    static int Process(string filename)
+    {
+        int counter = 0;
+
+        try
+        {
+            using ( StreamReader reader = new StreamReader(filename) )
+            {
+                string? line;
+                while ( (line = reader.ReadLine()) != null )
+                {
+                    foreach ( char ch in line )
+                    {
+                        switch ( ch )
+                        {
+                            case '(':
+                                counter++;
+                                break;
+                            case ')':
+                                counter--;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        catch ( IOException e )
+        {
+            Console.Error.WriteLine("Error reading file: " + e.Message);
+            Environment.Exit(1);
+        }
+
+        return counter;
+    }
+
+    static void Main(string[] args)
+    {
+        if ( args.Length < 1 )
+        {
+            Usage();
+        }
+
+        string filename = args[0];
+        int result = Process(filename);
+        Console.WriteLine("result = " + result);
+    }
+}
---
+++ csharp/2015/day01b/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Usage()
+    {
+        Console.Error.WriteLine("usage: dotnet run <input file>");
+        Environment.Exit(1);
+    }
+
+    static int Process(string filename)
+    {
+        int counter = 0;
+        int pos = 0;
+
+        try
+        {
+            using ( StreamReader reader = new StreamReader(filename) )
+            {
+                string? line;
+                while ( (line = reader.ReadLine()) != null )
+                {
+                    foreach ( char ch in line )
+                    {
+                        pos++;
+                        switch ( ch )
+                        {
+                            case '(':
+                                counter++;
+                                break;
+                            case ')':
+                                counter--;
+                                break;
+                        }
+                        if ( counter < 0 ) {
+                            return pos;
+                        }
+                    }
+                }
+            }
+        }
+        catch ( IOException e )
+        {
+            Console.Error.WriteLine("Error reading file: " + e.Message);
+            Environment.Exit(1);
+        }
+
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        if ( args.Length < 1 )
+        {
+            Usage();
+        }
+
+        string filename = args[0];
+        int result = Process(filename);
+        Console.WriteLine("result = " + result);
+    }
+}
