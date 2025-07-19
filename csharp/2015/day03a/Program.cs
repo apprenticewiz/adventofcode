928153373376d14f2baf35cbce66189637e7dc1e
Chris Parrott <chris@parrotts.net>
2025-07-19 14:57:10 -0700

    2015 day 3 in C#

---
+++ csharp/2015/day03a/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using AOC_Utils.Geometry;
+
+class Program
+{
+    static void Usage()
+    {
+        Console.Error.WriteLine("usage: dotnet run <input file>");
+        Environment.Exit(1);
+    }
+
+    static int Process(string filename)
+    {
+        HashSet<Position2D<int>> positions = new HashSet<Position2D<int>>();
+
+        Position2D<int> santa = new Position2D<int>(0 , 0);
+        positions.Add(santa);
+
+        try
+        {
+            using ( StreamReader reader = new StreamReader(filename) )
+            {
+                string? line;
+                while ( (line = reader.ReadLine()) != null )
+                {
+                    foreach ( char ch in line )
+                    {
+                        switch ( ch )
+                        {
+                            case '^':
+                                santa.Y += 1;
+                                break;
+                            case 'v':
+                                santa.Y -= 1;
+                                break;
+                            case '<':
+                                santa.X -= 1;
+                                break;
+                            case '>':
+                                santa.X += 1;
+                                break;
+                        }
+                        positions.Add(santa);
+                    }
+                }
+            }
+        }
+        catch ( IOException e )
+        {
+            Console.Error.WriteLine("Error reading file: " + e.Message);
+            Environment.Exit(1);
+        }
+
+        return positions.Count;
+    }
+
+    static void Main(string[] args)
+    {
+        if ( args.Length < 1 )
+        {
+            Usage();
+        }
+
+        string filename = args[0];
+        int result = Process(filename);
+        Console.WriteLine("result = " + result);
+    }
+}
---
+++ csharp/2015/day03b/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using AOC_Utils.Geometry;
+
+class Program
+{
+    static void Usage()
+    {
+        Console.Error.WriteLine("usage: dotnet run <input file>");
+        Environment.Exit(1);
+    }
+
+    static int Process(string filename)
+    {
+        HashSet<Position2D<int>> positions = new HashSet<Position2D<int>>();
+
+        Position2D<int> santa = new Position2D<int>(0 , 0);
+        Position2D<int> roboSanta = new Position2D<int>(0, 0);
+        positions.Add(santa);
+
+        try
+        {
+            using ( StreamReader reader = new StreamReader(filename) )
+            {
+                string? line;
+                int i = 0;
+                while ( (line = reader.ReadLine()) != null )
+                {
+                    foreach ( char ch in line )
+                    {
+                        if ( i % 2 == 0 ) {
+                            switch ( ch )
+                            {
+                                case '^':
+                                    santa.Y += 1;
+                                    break;
+                                case 'v':
+                                    santa.Y -= 1;
+                                    break;
+                                case '<':
+                                    santa.X -= 1;
+                                    break;
+                                case '>':
+                                    santa.X += 1;
+                                    break;
+                            }
+                            positions.Add(santa);
+                        }
+                        else
+                        {
+                            switch ( ch )
+                            {
+                                case '^':
+                                    roboSanta.Y += 1;
+                                    break;
+                                case 'v':
+                                    roboSanta.Y -= 1;
+                                    break;
+                                case '<':
+                                    roboSanta.X -= 1;
+                                    break;
+                                case '>':
+                                    roboSanta.X += 1;
+                                    break;
+                            }
+                            positions.Add(roboSanta);
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
+        catch ( IOException e )
+        {
+            Console.Error.WriteLine("Error reading file: " + e.Message);
+            Environment.Exit(1);
+        }
+
+        return positions.Count;
+    }
+
+    static void Main(string[] args)
+    {
+        if ( args.Length < 1 )
+        {
+            Usage();
+        }
+
+        string filename = args[0];
+        int result = Process(filename);
+        Console.WriteLine("result = " + result);
+    }
+}
