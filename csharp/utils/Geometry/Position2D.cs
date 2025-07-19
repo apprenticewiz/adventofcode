9214cb6c78ae1623276b788d1ab22a85b0c02067
Chris Parrott <chris@parrotts.net>
2025-07-19 14:55:58 -0700

    create new Geometry package in utilities with Position2D and Position3D structs

---
+++ csharp/utils/Geometry/Position2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace AOC_Utils.Geometry;
+
+public struct Position2D<T> : IEquatable<Position2D<T>>
+    where T : INumber<T>
+{
+    public T X { get; set; }
+    public T Y { get; set; }
+
+    public Position2D(T x, T y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Position2D<T> Add(Position2D<T> other)
+    {
+        return new Position2D<T>(X + other.X, Y + other.Y);
+    }
+
+    public override bool Equals(object? obj) => obj is Position2D<T> other && Equals(other);
+    public bool Equals(Position2D<T> other) => X == other.X && Y == other.Y;
+
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public override string ToString() => $"Position2D({X}, {Y})";
+}
---
+++ csharp/utils/Geometry/Position3D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace AOC_Utils.Geometry;
+
+public struct Position3D<T> : IEquatable<Position3D<T>>
+    where T : INumber<T>
+{
+    public T X { get; set; }
+    public T Y { get; set; }
+    public T Z { get; set; }
+
+    public Position3D(T x, T y, T z)
+    {
+        X = x;
+        Y = y;
+	Z = z;
+    }
+
+    public Position3D<T> Add(Position3D<T> other)
+    {
+        return new Position3D<T>(X + other.X, Y + other.Y, Z + other.Z);
+    }
+
+    public override bool Equals(object? obj) => obj is Position3D<T> other && Equals(other);
+    public bool Equals(Position3D<T> other) => X == other.X && Y == other.Y && Z == other.Z;
+
+    public override int GetHashCode() => HashCode.Combine(X, Y, Z);
+    public override string ToString() => $"Position3D({X}, {Y}, {Z})";
+}
