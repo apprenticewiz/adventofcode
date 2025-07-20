ad642ef6b793a4d1a2531aea6f3a07b7ecbdfcb5
Chris Parrott <chris@parrotts.net>
2025-07-20 00:22:21 -0700

    fix typo in Position3D declaration

--- zig/utils/geometry/src/root.zig
+++ zig/utils/geometry/src/root.zig
@@ -2,5 +2,5 @@ const position2d = @import("position2d.zig");
 const position3d = @import("position3d.zig");
 
 pub const Position2D = position2d.Position2D;
-pub const Position3D = position2d.Position3D;
+pub const Position3D = position3d.Position3D;
 
