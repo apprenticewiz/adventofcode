9ee39dc63d9ff6fd7b8d6d0fcf68a488d08e7884
Chris Parrott <chris@parrotts.net>
2025-07-20 18:28:21 -0700

    2015 day 4 in Zig

---
+++ zig/2015/day04a/src/main.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+fn usage(progname: []const u8) void {
+    std.debug.print("usage: {s} <key>\n", .{progname});
+    std.process.exit(1);
+}
+
+fn processFile(allocator: std.mem.Allocator, key: []const u8) !u32 {
+    var n: u32 = 1;
+
+    var buf = std.ArrayList(u8).init(allocator);
+    defer buf.deinit();
+
+    while ( true ) {
+        const b = try std.fmt.allocPrint(allocator, "{s}{d}", .{key, n});
+        defer allocator.free(b);
+
+        const md5 = std.crypto.hash.Md5;
+        var out: [md5.digest_length]u8 = undefined;
+        md5.hash(b, &out, .{});
+
+        const hex = try std.fmt.allocPrint(allocator, "{}", .{std.fmt.fmtSliceHexLower(&out)});
+        defer allocator.free(hex);
+
+        if ( std.mem.startsWith(u8, hex, "00000") ) {
+            return n;
+        }
+        n += 1;
+    }
+
+    return 0;
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const stdout = std.io.getStdOut().writer();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if ( args.len < 2 ) {
+        usage(args[0]);
+    }
+
+    const filename = args[1];
+
+    const result = processFile(allocator, filename) catch |err| {
+        std.debug.print("error while processing file `{s}': {s}\n", .{filename, @errorName(err)});
+        std.process.exit(1);
+    };
+
+    try stdout.print("result = {}\n", .{result});
+}
