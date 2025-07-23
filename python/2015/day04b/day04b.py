ab1c66bbe1f6ca2e059df0afbe6b11efffe62f6e
Chris Parrott <chris@parrotts.net>
2025-07-23 02:17:37 -0700

    2015 day 4 in Python

---
+++ python/2015/day04b/day04b.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import hashlib
+import sys
+
+def usage():
+    progname = sys.argv[0]
+    print(f'usage: {progname} <key>')
+    sys.exit(1)
+
+def process(key):
+    n = 1
+    while True:
+        tryKey = key + str(n)
+        md5 = hashlib.md5()
+        md5.update(tryKey.encode('utf-8'))
+        digest = md5.hexdigest()
+        if digest.startswith('000000'):
+            return n
+        n += 1
+
+def main():
+    if len(sys.argv) < 2:
+        usage()
+    key = sys.argv[1]
+    result = process(key)
+    print(f'result = {result}')
+
+if __name__ == '__main__':
+    main()
