af3f647f4e007d31b44cc4b0b8f9da67e9fdc0b2
Chris Parrott <chris@parrotts.net>
2025-07-24 12:55:47 -0700

    2015 day 4 in Perl

---
+++ perl/2015/day04a/day04a.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Digest::MD5 qw(md5_hex);
+
+sub usage {
+    my $progname = $0;
+    print "usage: $progname <key>\n";
+    exit 1;
+}
+
+sub process {
+    my ($key) = @_;
+    my $n = 1;
+    while ( 1 ) {
+        my $tryKey = $key . $n;
+        my $digest = md5_hex($tryKey);
+        if ( $digest =~ /^00000/ ) {
+            return $n;
+        } else {
+            $n++;
+        }
+    }
+}
+
+sub main {
+    if ( @ARGV < 1 ) {
+        usage();
+    }
+    my $key = $ARGV[0];
+    my $result = process($key);
+    print "result = $result\n";
+}
+
+main();
