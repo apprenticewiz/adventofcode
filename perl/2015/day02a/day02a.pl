649931c66f440f9f5d1713a4b11ebe72226478f7
Chris Parrott <chris@parrotts.net>
2025-07-24 12:28:22 -0700

    formatting fixes

--- perl/2015/day02a/day02a.pl
+++ perl/2015/day02a/day02a.pl
@@ -17,13 +17,13 @@ sub process {
 
     open ( my $infile, '<', $filename ) or die "cannot open file: $filename: $!";
     while ( my $line = <$infile> ) {
-	    my ($l, $w, $h) = map { $_ + 0 } split(/x/, $line);
-	    my $area1 = $l * $w;
-	    my $area2 = $l * $h;
-	    my $area3 = $w * $h;
-	    my $surfaceArea = (2 * $area1) + (2 * $area2) + (2 * $area3);
-	    my $minArea = min($area1, $area2, $area3);
-	    $totalArea += $surfaceArea + $minArea;
+        my ($l, $w, $h) = map { $_ + 0 } split(/x/, $line);
+        my $area1 = $l * $w;
+        my $area2 = $l * $h;
+        my $area3 = $w * $h;
+        my $surfaceArea = (2 * $area1) + (2 * $area2) + (2 * $area3);
+        my $minArea = min($area1, $area2, $area3);
+        $totalArea += $surfaceArea + $minArea;
     }
     close($infile);
 
