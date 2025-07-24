192f9fca1cc6a4e621d5bd1457039219065d2577
Chris Parrott <chris@parrotts.net>
2025-07-24 00:56:25 -0700

    2015 day 5 in JavaScript

---
+++ javascript/2015/day05a/day05a.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+function usage() {
+    const progname = path.basename(process.argv[1]);
+    console.log(`usage: ${progname} <input file>`);
+    process.exit(1);
+}
+
+function prop1(str) {
+    const vowels = str.match(/[aeiou]/g);
+    return vowels && vowels.length >= 3;
+}
+
+function prop2(str) {
+    return /(.)\1/.test(str);
+}
+
+function prop3(str) {
+    return !(/(ab|cd|pq|xy)/.test(str));
+}
+
+function processFile(filename) {
+    let count = 0;
+    const data = fs.readFileSync(filename, 'utf8');
+    const lines = data.trim().split('\n');
+    for ( const line of lines ) {
+        if ( prop1(line) && prop2(line) && prop3(line) ) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function main() {
+    if ( process.argv.length < 3 ) {
+        usage();
+    }
+    const filename = process.argv[2];
+    const result = processFile(filename);
+    console.log(`result = ${result}`);
+}
+
+main()
