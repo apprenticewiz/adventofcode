30caebe83ad470b29c5c0fa65054f5b5a17f72fc
Chris Parrott <chris@parrotts.net>
2025-07-26 23:18:59 -0700

    formatting fixes

--- ada/2015/day01b/day01b.adb
+++ ada/2015/day01b/day01b.adb
@@ -1,6 +1,6 @@
+with Ada.Command_Line;          use Ada.Command_Line;
 with Ada.Exceptions;            use Ada.Exceptions;
 with Ada.Text_IO;               use Ada.Text_IO;
-with Ada.Command_Line;          use Ada.Command_Line;
 with Ada.Integer_Text_IO;       use Ada.Integer_Text_IO;
 with GNAT.OS_Lib;
 
