08ce95171f2ad9b77407ed4e82216cdb0f21f31d
Chris Parrott <chris@parrotts.net>
2025-07-29 23:50:14 -0700

    2015 day 2 in D

---
+++ d/2015/day02a/day02a.d
@@ -0,0 +1,50 @@
+import std.algorithm;
+import std.conv;
+import std.exception;
+import std.file;
+import std.regex;
+import std.stdio;
+
+void usage(string progname) {
+  stderr.writeln("usage: ", progname, " <input file>");
+}
+
+int process(string filename) {
+  int totalArea = 0;
+
+  foreach ( line; File(filename).byLine() ) {
+    auto dims = split(line, regex(`x`));
+    int l = to!int(dims[0]);
+    int w = to!int(dims[1]);
+    int h = to!int(dims[2]);
+    int area1 = l * w;
+    int area2 = l * h;
+    int area3 = w * h;
+    int surfaceArea = 2 * area1 + 2 * area2 + 2 * area3;
+    int minArea = [area1, area2, area3].minElement;
+    totalArea += surfaceArea + minArea;
+  }
+
+  return totalArea;
+}
+
+int main(string[] args) {
+    string progname = args[0];
+
+    if ( args.length < 2 ) {
+        usage(progname);
+    return 1;
+    }
+
+    string filename = args[1];
+
+    try {
+        int result = process(filename);
+        writeln("result = ", result);
+    } catch ( Exception e ) {
+        stderr.writeln("error while processing file `", filename, "`: `", e.msg);
+    return 1;
+    }
+
+    return 0;
+}
---
+++ d/2015/day02b/day02b.d
@@ -0,0 +1,50 @@
+import std.algorithm;
+import std.conv;
+import std.exception;
+import std.file;
+import std.regex;
+import std.stdio;
+
+void usage(string progname) {
+  stderr.writeln("usage: ", progname, " <input file>");
+}
+
+int process(string filename) {
+  int totalLen = 0;
+
+  foreach ( line; File(filename).byLine() ) {
+    auto dims = split(line, regex(`x`));
+    int l = to!int(dims[0]);
+    int w = to!int(dims[1]);
+    int h = to!int(dims[2]);
+    int perim1 = 2 * (l + w);
+    int perim2 = 2 * (l + h);
+    int perim3 = 2 * (w + h);
+    int presentLen = [perim1, perim2, perim3].minElement;
+    int bowLen = l * w * h;
+    totalLen += presentLen + bowLen;
+  }
+
+  return totalLen;
+}
+
+int main(string[] args) {
+    string progname = args[0];
+
+    if ( args.length < 2 ) {
+        usage(progname);
+    return 1;
+    }
+
+    string filename = args[1];
+
+    try {
+        int result = process(filename);
+        writeln("result = ", result);
+    } catch ( Exception e ) {
+        stderr.writeln("error while processing file `", filename, "`: `", e.msg);
+    return 1;
+    }
+
+    return 0;
+}
