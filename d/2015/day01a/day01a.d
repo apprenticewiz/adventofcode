eef490ba0d4f5cca3017e88bdc50218736eafa9d
Chris Parrott <chris@parrotts.net>
2025-07-29 23:26:57 -0700

    2015 day 1 in D

---
+++ d/2015/day01a/day01a.d
@@ -0,0 +1,52 @@
+import std.conv;
+import std.exception;
+import std.file;
+import std.process;
+import std.stdio;
+import std.string;
+
+void usage(string progname) {
+  stderr.writeln("usage: ", progname, " <input file>");
+}
+
+int process(string filename) {
+  int floors = 0;
+
+  foreach ( line; File(filename).byLine() ) {
+      foreach ( ch; line ) {
+          switch ( ch ) {
+              case '(':
+                  floors += 1;
+                  break;
+              case ')':
+                  floors -= 1;
+                  break;
+            default:
+                  break;
+          }
+      }
+  }
+
+  return floors;
+}
+
+int main(string[] args) {
+    string progname = args[0];
+
+    if ( args.length < 2 ) {
+        usage(progname);
+	return 1;
+    }
+
+    string filename = args[1];
+
+    try {
+        int result = process(filename);
+        writeln("result = ", result);
+    } catch ( Exception e ) {
+        stderr.writeln("error while processing file `", filename, "`: `", e.msg);
+	return 1;
+    }
+
+    return 0;
+}
