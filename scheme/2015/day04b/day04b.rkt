221f42fe6bb3d48af97fcb4813ecdc140572e40f
Chris Parrott <chris@parrotts.net>
2025-07-30 18:29:44 -0700

    2015 day 4 in Scheme (racket)

---
+++ scheme/2015/day04b/day04b.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(require file/sha1 
+         openssl/md5
+         racket/cmdline
+         racket/port
+         racket/string
+         racket/system)
+
+(define (usage progname)
+  (displayln (string-append "usage: " progname " <key>") (current-error-port))
+  (exit 1))
+
+(define (check-key key n)
+  (define try-key (string-append key (number->string n)))
+  (define digest (md5-bytes (open-input-bytes (string->bytes/utf-8 try-key))))
+  (define hex-digest (bytes->hex-string digest))
+  (if (string-prefix? hex-digest "000000")
+    n
+    (check-key key (+ n 1))))
+
+(define (process key)
+  (check-key key 1))
+
+(define (main)
+  (define progname (find-system-path 'run-file))
+  (define args (current-command-line-arguments))
+  (cond
+    [(= (vector-length args) 1)
+      (define key (vector-ref args 0))
+      (define result (process key))
+      (printf "result = ~a\n" result)]
+    [else (usage (path->string progname))]))
+
+(main)
