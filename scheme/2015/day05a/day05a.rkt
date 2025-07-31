edc783263ee9c2b1bc6407d2c478a9c7444e011a
Chris Parrott <chris@parrotts.net>
2025-07-30 18:55:25 -0700

    2015 day 5 in Scheme (racket)

---
+++ scheme/2015/day05a/day05a.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(require racket/cmdline
+         racket/port
+         racket/system)
+
+(define (usage progname)
+  (displayln (string-append "usage: " progname " <input file>") (current-error-port))
+  (exit 1))
+
+(define (prop1 str)
+  (>= (length (regexp-match* #px"[aeiou]" str)) 3))
+
+(define (prop2 str)
+  (regexp-match? #px"(.)\\1" str))
+
+(define (prop3 str)
+  (not (regexp-match? #px"ab|cd|pq|xy" str)))
+
+(define (process content)
+  (foldl
+    (lambda (line acc)
+      (if (and (prop1 line) (prop2 line) (prop3 line))
+        (+ acc 1)
+        acc))
+   0
+   (string-split content)))
+
+(define (main)
+  (define progname (find-system-path 'run-file))
+  (define args (current-command-line-arguments))
+  (cond
+    [(= (vector-length args) 1)
+      (define filename (vector-ref args 0))
+      (define content (file->string filename))
+      (define result (process content))
+      (printf "result = ~a\n" result)]
+    [else (usage (path->string progname))]))
+
+(main)
