2dd93efef981aec63221b169740a3d739679fe23
Chris Parrott <chris@parrotts.net>
2025-08-01 12:22:35 -0700

    2015 day 1 in Kotlin

---
+++ kotlin/2015/day01a/Program.kt
@@ -0,0 +1,35 @@
+import java.io.File;
+import kotlin.system.*;
+
+fun usage() {
+    System.err.println("usage: java -jar Program.jar <input file>")
+    kotlin.system.exitProcess(1)
+}
+
+fun process(content: String) : Int {
+    var floors = 0;
+
+    for ( ch in content ) {
+        when ( ch ) {
+            '(' -> floors += 1
+            ')' -> floors -= 1
+            else -> {}
+        }
+    }
+
+    return floors;
+}
+
+fun main(args: Array<String>) {
+    if ( args.isEmpty()) {
+        usage()
+    }
+    val filename = args[0]
+    try {
+        val content = File(filename).readText()
+        val result = process(content)
+        println("result = $result")
+    } catch ( e: Exception ) {
+        System.err.println("error reading file: ${e.message}")
+    }
+}
---
+++ kotlin/2015/day01b/Program.kt
@@ -0,0 +1,40 @@
+import java.io.File;
+import kotlin.system.*;
+
+fun usage() {
+    System.err.println("usage: java -jar Program.jar <input file>")
+    kotlin.system.exitProcess(1)
+}
+
+fun process(content: String) : Int {
+    var floors = 0;
+    var pos = 0;
+
+    for ( ch in content ) {
+        pos += 1;
+        when ( ch ) {
+            '(' -> floors += 1
+            ')' -> floors -= 1
+            else -> {}
+        }
+        if ( floors < 0 ) {
+            return pos;
+        }
+    }
+
+    return 0;
+}
+
+fun main(args: Array<String>) {
+    if ( args.isEmpty()) {
+        usage()
+    }
+    val filename = args[0]
+    try {
+        val content = File(filename).readText()
+        val result = process(content)
+        println("result = $result")
+    } catch ( e: Exception ) {
+        System.err.println("error reading file: ${e.message}")
+    }
+}
