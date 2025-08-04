4c42a8b84f1374416f3e514c14ed99039d74b3c6
Chris Parrott <chris@parrotts.net>
2025-08-04 00:42:44 -0700

    2015 day 1 in F#

---
+++ fsharp/2015/day01a/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+
+exception PositionFound of int
+
+let usage() =
+    Console.Error.WriteLine("usage: dotnet run <input file>")
+    Environment.Exit(1)
+
+let processFile(filename: string) =
+    let f count ch =
+        match ch with
+        | '(' -> count + 1
+        | ')' -> count - 1
+        | _ -> count
+
+    try
+        File.ReadAllText(filename)
+        |> Seq.fold f 0
+    with
+    | :? IOException as e ->
+        Console.Error.WriteLine("Error reading file: " + e.Message)
+        Environment.Exit(1)
+        0
+
+[<EntryPoint>]
+let main args =
+    if args.Length < 1 then
+        usage()
+
+    let filename = args.[0]
+    let result = processFile filename
+    Console.WriteLine("result = " + result.ToString());
+    0 
---
+++ fsharp/2015/day01b/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+open System.IO
+
+exception PositionFound of int
+
+let usage() =
+    Console.Error.WriteLine("usage: dotnet run <input file>")
+    Environment.Exit(1)
+
+let processFile(filename: string) =
+    let f (pos, count) ch =
+        if count < 0 then (pos, count)
+        else
+            let count' =
+                match ch with
+                | '(' -> count + 1
+                | ')' -> count - 1
+                | _ -> count
+            (pos + 1, count')
+
+    try
+        File.ReadAllText(filename)
+        |> Seq.fold f (0, 0)
+        |> fst
+    with
+    | :? IOException as e ->
+        Console.Error.WriteLine("Error reading file: " + e.Message)
+        Environment.Exit(1)
+        0
+
+[<EntryPoint>]
+let main args =
+    if args.Length < 1 then
+        usage()
+
+    let filename = args.[0]
+    let result = processFile filename
+    Console.WriteLine("result = " + result.ToString());
+    0 
