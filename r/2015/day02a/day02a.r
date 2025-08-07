1c56da960799aaf229bc0267383a32b164f2f74d
Chris Parrott <chris@parrotts.net>
2025-08-06 22:03:04 -0700

    2015 day 2 in R

---
+++ r/2015/day02a/day02a.r
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+
+usage <- function() {
+    args <- commandArgs()
+    progname <- sub("^--file=", "", args[grep("^--file=", args)])
+    cat("usage:", progname, "<input file>\n")
+    quit(status = 1)
+}
+
+process <- function(filename) {
+    totalArea <- 0
+    lines <- suppressWarnings(readLines(filename))
+    for ( line in lines ) {
+        dims <- as.integer(strsplit(line, "x")[[1]])
+        l <- dims[1]
+        w <- dims[2]
+        h <- dims[3]
+	area1 <- l * w
+	area2 <- l * h
+	area3 <- w * h
+        surfaceArea <- 2 * area1 + 2 * area2 + 2 * area3
+	minArea <- min(area1, area2, area3)
+	totalArea <- totalArea + surfaceArea + minArea
+    }
+    return(totalArea)
+}
+
+main <- function() {
+    args <- commandArgs(trailingOnly = TRUE)
+    if ( length(args) < 1 ) {
+        usage()
+    }
+    filename <- args[1]
+    result <- process(filename)
+    cat("result =", result, "\n")
+}
+
+main()
---
+++ r/2015/day02b/day02b.r
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+
+usage <- function() {
+    args <- commandArgs()
+    progname <- sub("^--file=", "", args[grep("^--file=", args)])
+    cat("usage:", progname, "<input file>\n")
+    quit(status = 1)
+}
+
+process <- function(filename) {
+    totalLen <- 0
+    lines <- suppressWarnings(readLines(filename))
+    for ( line in lines ) {
+        dims <- as.integer(strsplit(line, "x")[[1]])
+        l <- dims[1]
+        w <- dims[2]
+        h <- dims[3]
+	perim1 <- 2 * (l + w)
+	perim2 <- 2 * (l + h)
+	perim3 <- 2 * (w + h)
+	presentLen <- min(perim1, perim2, perim3)
+        bowLen <- l * w * h
+	totalLen <- totalLen + presentLen + bowLen
+    }
+    return(totalLen)
+}
+
+main <- function() {
+    args <- commandArgs(trailingOnly = TRUE)
+    if ( length(args) < 1 ) {
+        usage()
+    }
+    filename <- args[1]
+    result <- process(filename)
+    cat("result =", result, "\n")
+}
+
+main()
