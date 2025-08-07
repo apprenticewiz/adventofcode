3e9ea19b5055baebb2c7658513f4869a728b3034
Chris Parrott <chris@parrotts.net>
2025-08-06 23:03:28 -0700

    2015 day 4 in R

---
+++ r/2015/day04a/day04a.r
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+library(digest)
+
+usage <- function() {
+    args <- commandArgs()
+    progname <- sub("^--file=", "", args[grep("^--file=", args)])
+    cat("usage:", progname, "<input file>\n")
+    quit(status = 1)
+}
+
+process <- function(key) {
+    n <- 0
+    repeat {
+        tryKey <- paste0(key, n)
+        hexDigest <- digest(tryKey, algo = "md5", serialize = FALSE)
+        if ( startsWith(hexDigest, "00000") ) {
+            return(n)
+        }
+        n <- n + 1
+    }
+}
+
+main <- function() {
+    args <- commandArgs(trailingOnly = TRUE)
+    if ( length(args) < 1 ) {
+        usage()
+    }
+    key <- args[1]
+    result <- process(key)
+    cat("result =", result, "\n")
+}
+
+main()
