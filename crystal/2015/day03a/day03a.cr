cc75a1ff061517097836589360afa0ba8692fec5
Chris Parrott <chris@parrotts.net>
2025-08-11 01:38:04 -0700

    2015 day 3 in Crystal

---
+++ crystal/2015/day03a/day03a.cr
@@ -0,0 +1,35 @@
+def usage
+    puts "usage: #{PROGRAM_NAME} <input file>"
+    exit 1
+end
+
+def process(filename)
+    santa = {0, 0}
+    positions = Set({Int32, Int32}).new
+    File.each_line(filename) do |line|
+        line.each_char do |ch|
+            case ch
+            when '^'
+                santa = {santa[0], santa[1] + 1}
+            when 'v'
+                santa = {santa[0], santa[1] - 1}
+            when '<'
+                santa = {santa[0] - 1, santa[1]}
+            when '>'
+                santa = {santa[0] + 1, santa[1]}
+            end
+            positions.add(santa)
+        end
+    end
+    positions.size
+end
+
+def main
+    usage if ARGV.size < 1
+    filename = ARGV[0]
+    result = process(filename)
+    puts "result = #{result}"
+end
+
+main
+
---
+++ crystal/2015/day03b/day03b.cr
@@ -0,0 +1,45 @@
+def usage
+    puts "usage: #{PROGRAM_NAME} <input file>"
+    exit 1
+end
+
+def process(filename)
+    santa = {0, 0}
+    robo_santa = {0, 0}
+    santa_move = true
+    positions = Set({Int32, Int32}).new
+    File.each_line(filename) do |line|
+        line.each_char do |ch|
+            case ch
+            when '^'
+               new_pos = santa_move ? {santa[0], santa[1] + 1} : {robo_santa[0], robo_santa[1] + 1}
+            when 'v'
+               new_pos = santa_move ? {santa[0], santa[1] - 1} : {robo_santa[0], robo_santa[1] - 1}
+            when '<'
+               new_pos = santa_move ? {santa[0] - 1, santa[1]} : {robo_santa[0] - 1, robo_santa[1]}
+            when '>'
+               new_pos = santa_move ? {santa[0] + 1, santa[1]} : {robo_santa[0] + 1, robo_santa[1]}
+            else
+               next
+            end
+            positions.add(new_pos)
+            if santa_move
+                santa = new_pos
+            else
+                robo_santa = new_pos
+            end
+            santa_move = !santa_move
+        end
+    end
+    positions.size
+end
+
+def main
+    usage if ARGV.size < 1
+    filename = ARGV[0]
+    result = process(filename)
+    puts "result = #{result}"
+end
+
+main
+
