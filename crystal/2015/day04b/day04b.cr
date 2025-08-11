44afcedc9bf4f40f1c6da5b0c5216566c4ce6414
Chris Parrott <chris@parrotts.net>
2025-08-11 01:42:32 -0700

    2015 day 4 in Crystal

---
+++ crystal/2015/day04b/day04b.cr
@@ -0,0 +1,29 @@
+require "digest/md5"
+
+def usage
+    puts "usage: #{PROGRAM_NAME} <key>"
+    exit 1
+end
+
+def process(key)
+    n = 1
+    while true
+        try_key = key + n.to_s
+        digest = Digest::MD5.hexdigest(try_key)
+        if digest.starts_with?("000000")
+            return n
+        else
+            n += 1
+        end
+    end
+end
+
+def main
+    usage if ARGV.size < 1
+    key = ARGV[0]
+    result = process(key)
+    puts "result = #{result}"
+end
+
+main
+
