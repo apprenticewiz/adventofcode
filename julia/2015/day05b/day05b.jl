99aac935af2b937e91e3d047d8501f07302b985e
Chris Parrott <chris@parrotts.net>
2025-08-11 19:36:17 -0700

    2015 day 5 in Julia

---
+++ julia/2015/day05b/day05b.jl
@@ -0,0 +1,45 @@
+#!/usr/bin/env julia
+
+function usage()
+    progname = PROGRAM_FILE
+    println(stderr, "usage: $progname <input file>")
+    exit(1)
+end
+
+function prop1(line::String)::Bool
+    return occursin(r"(..).*\1", line)
+end
+
+function prop2(line::String)::Bool
+    return occursin(r"(.).\1", line)
+end
+
+function process(content::String)::Int32
+    res = 0
+    for line in split(content, '\n', keepempty=false)
+        if prop1(String(line)) && prop2(String(line))
+            res += 1
+        end
+    end
+    return Int32(res)
+end
+
+function main()
+    args = ARGS
+    if length(args) < 1
+        usage()
+    end
+    filename = args[1]
+    try
+        content = read(filename, String)
+        result = process(content)
+        println("result = $result")
+    catch e
+        println(stderr, "error reading file: $e")
+        exit(1)
+    end
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
