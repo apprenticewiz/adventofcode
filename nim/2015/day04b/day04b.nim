6dec554955c881806217ceec80a764462d46b6c0
Chris Parrott <chris@parrotts.net>
2025-08-17 23:53:31 -0700

    2015 day 4 in Nim

---
+++ nim/2015/day04b/day04b.nim
@@ -0,0 +1,17 @@
+import checksums/md5, os, strutils
+
+proc processKey(key: string): int =
+  inc result
+  while true:
+    let tryKey = key & $result
+    let digest = getMD5(tryKey)
+    if digest.startsWith("000000"):
+      break
+    inc result
+
+when isMainModule:
+  if paramCount() < 1:
+    quit "usage: " & getAppFilename() & " <key>", 1
+
+  let key = paramStr(1)
+  echo "result = ", processKey(key)
