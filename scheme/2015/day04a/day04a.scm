1965fe5bb7adb4a560b4989a5eb7f5fba57750ec
Chris Parrott <chris@parrotts.net>
2025-08-20 14:12:09 -0700

    2015 day 4 in Scheme

---
+++ scheme/2015/day04a/day04a.scm
@@ -0,0 +1,24 @@
+(module day04a
+  (main main))
+
+(define (main args)
+  (let* ((argc (length args))
+         (progname (car args)))
+    (if (< argc 2)
+      (usage progname))
+      (let* ((key (cadr args))
+             (result (process key)))
+        (printf "result = ~a\n" (number->string result)))))
+
+(define (usage progname)
+  (fprintf (current-error-port) "usage: ~a <key>\n" progname)
+  (exit 1))
+
+(define (process key)
+  (define (check-key k n)
+    (let* ((try-key (string-append k (number->string n)))
+	   (digest (md5sum-string try-key)))
+      (if (string-prefix? "00000" digest)
+	n
+	(check-key k (+ n 1)))))
+  (check-key key 0))
