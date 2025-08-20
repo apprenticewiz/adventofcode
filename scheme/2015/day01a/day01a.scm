6ae1156419e86cc6cc208507bc7cfc5acd9b2428
Chris Parrott <chris@parrotts.net>
2025-08-20 01:36:57 -0700

    Reworked day 1 with fold-left.  Bigloo does not provide fold-left, so we roll our own.

--- scheme/2015/day01a/day01a.scm
+++ scheme/2015/day01a/day01a.scm
@@ -14,19 +14,22 @@
   (fprintf (current-error-port) "usage: ~a <input file>\n" progname)
   (exit 1))
 
+(define (fold-left op u l)
+  (if (null? l)
+    u
+    (let ((x (car l))
+	  (xs (cdr l)))
+      (fold-left op (op u x) xs))))
+
 (define (process filename)
   (let* ((input-file (open-input-file filename))
          (contents (read-string input-file)))
-    (define (calc-floor current-floor xs)
-      (if (null? xs)
-        current-floor
-        (let* ((ch (car xs))
-               (rest (cdr xs))
-               (next-floor
-                 (cond
-                   ((char=? ch #\() (+ current-floor 1))
-                   ((char=? ch #\)) (- current-floor 1))
-                   (else current-floor))))
-          (calc-floor next-floor rest))))
     (close-input-port input-file)
-    (calc-floor 0 (string->list contents))))
+    (fold-left
+      (lambda (current-floor ch)
+	(cond
+	  ((char=? ch #\() (+ current-floor 1))
+	  ((char=? ch #\)) (- current-floor 1))
+	  (else current-floor)))
+      0
+      (string->list contents))))
