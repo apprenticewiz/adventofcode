fb1f3a408dfd550eaf0d25983741cae443478527
Chris Parrott <chris@parrotts.net>
2025-08-20 16:42:25 -0700

    2015 day 4 in Tcl

---
+++ tcl/2015/day04b/day04b.tcl
@@ -0,0 +1,33 @@
+#!/usr/bin/env tclsh
+
+package require md5
+
+proc usage {} {
+    set progname $::argv0
+    puts stderr "usage: $progname <key>"
+    exit 1
+}
+
+proc process {key} {
+    set n 1
+    while { 1 } {
+        set try_key "$key$n"
+	set digest [string tolower [::md5::md5 -hex $try_key]]
+        if { [string equal [string range $digest 0 5] "000000"] } {
+            break
+        }
+        incr n
+    }
+    return $n
+}
+
+proc main {} {
+    if { [llength $::argv] < 1} {
+        usage
+    }
+    set key [lindex $::argv 0]
+    set result [process $key]
+    puts "result = $result"
+}
+
+main
