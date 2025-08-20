a7a961c4f8d5288e841cdb6d5f0006a633a5892b
Chris Parrott <chris@parrotts.net>
2025-08-20 14:13:40 -0700

    fix formatting

--- scheme/2015/day04a/day04a.scm
+++ scheme/2015/day04a/day04a.scm
@@ -17,8 +17,8 @@
 (define (process key)
   (define (check-key k n)
     (let* ((try-key (string-append k (number->string n)))
-	   (digest (md5sum-string try-key)))
+           (digest (md5sum-string try-key)))
       (if (string-prefix? "00000" digest)
-	n
-	(check-key k (+ n 1)))))
+        n
+        (check-key k (+ n 1)))))
   (check-key key 0))
