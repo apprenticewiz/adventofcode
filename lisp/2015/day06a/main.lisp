2862e45b3bc9cd1d8505ac149b183030d2b2f6aa
Chris Parrott <chris@parrotts.net>
2025-08-22 22:23:38 -0700

    2015 day 6 in Lisp

---
+++ lisp/2015/day06a/main.lisp
@@ -0,0 +1,57 @@
+#!/usr/bin/sbcl --script
+
+(load "~/quicklisp/setup.lisp")
+
+(ql:quickload "cl-ppcre")
+
+(defun usage ()
+  (format *error-output* "usage: main.lisp <input file>~%")
+  (quit :unix-status 1))
+
+(defun perform (grid action r1 c1 r2 c2)
+  (dotimes (i (+ (- r2 r1) 1))
+    (dotimes (j (+ (- c2 c1) 1))
+      (let* ((row (+ r1 i))
+             (col (+ c1 j))
+             (old-value (aref grid row col)))
+        (cond ((string= action "turn on") (setf (aref grid row col) t))
+              ((string= action "turn off") (setf (aref grid row col) nil))
+              ((string= action "toggle") (setf (aref grid row col) (not old-value))))))))
+
+(defun count-values (grid)
+  (let ((total 0))
+    (dotimes (row 1000)
+      (dotimes (col 1000)
+        (if (aref grid row col)
+          (setq total (+ 1 total)))))
+          total))
+
+(defun process (filename)
+  (with-open-file (infile filename :direction :input)
+    (let ((grid (make-array '(1000 1000) :initial-element nil)))
+      (loop for line = (read-line infile nil)
+            while line
+            do
+              (let*
+                ((result (multiple-value-list
+                           (cl-ppcre:scan-to-strings
+                             "(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)"
+                              line)))
+                 (tokens (second result))
+                 (action (aref tokens 0))
+                 (r1 (first (multiple-value-list (parse-integer (aref tokens 1)))))
+                 (c1 (first (multiple-value-list (parse-integer (aref tokens 2)))))
+                 (r2 (first (multiple-value-list (parse-integer (aref tokens 3)))))
+                 (c2 (first (multiple-value-list (parse-integer (aref tokens 4))))))
+               (perform grid action r1 c1 r2 c2)))
+      (count-values grid))))
+
+(defun main ()
+  (let ((args (cdr *posix-argv*)))
+    (if (= (length args) 1)
+        (let* ((filename (first args))
+               (result (process filename)))
+          (format t "result = ~A~%" result))
+        (usage))))
+
+(main)
