33ea31e468892c178324a4226c2e61a7160198e9
Chris Parrott <chris@parrotts.net>
2025-08-24 18:12:54 -0700

    2015 day 6 in Erlang

---
+++ erlang/2015/day06b/Program.erl
@@ -0,0 +1,56 @@
+#!/usr/bin/env escript
+
+main(Args) ->
+  case Args of
+    [Filename] ->
+      case process(Filename) of
+        {ok, Result} ->
+          io:format("result = ~p~n", [Result]);
+        {error, Reason} ->
+          io:format(standard_error, "error while processing file `~s': ~p~n", [Filename, Reason]),
+          halt(1)
+      end;
+    _ ->
+      usage()
+  end.
+
+usage() ->
+  io:format(standard_error, "usage: Program.erl <input file>~n", []),
+  halt(1).
+
+perform(Grid, Action, R1, C1, R2, C2) ->
+  Indices = [{Rows, Cols} || Rows <- lists:seq(R1, R2), Cols <- lists:seq(C1, C2)],
+  lists:foldl(
+    fun ({Row, Col}, CurrGrid) ->
+      OldValue = maps:get({Row, Col}, CurrGrid, 0),
+      case Action of
+        "turn on" -> maps:put({Row, Col}, OldValue + 1, CurrGrid);
+        "turn off" -> maps:put({Row, Col}, max(0, OldValue - 1), CurrGrid);
+        "toggle" -> maps:put({Row, Col}, OldValue + 2, CurrGrid)
+      end
+    end,
+    Grid,
+    Indices).
+
+process(Filename) ->
+  case file:read_file(Filename) of
+    {ok, Chars} ->
+      Lines = string:lexemes(Chars, "\n"),
+      Grid = lists:foldl(
+        fun
+          (Line, CurrGrid) ->
+            case re:run(Line, "(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)",
+                        [unicode, {capture, all_but_first, list}]) of
+              {match, [Action, R1S, C1S, R2S, C2S]} ->
+                perform(CurrGrid, Action, list_to_integer(R1S), list_to_integer(C1S),
+                list_to_integer(R2S), list_to_integer(C2S));
+              nomatch -> skip
+            end
+        end,
+	#{},
+        Lines),
+      Result = maps:fold(fun (_, Val, Acc) -> Acc + Val end, 0, Grid),
+      {ok, Result};
+    {error, Reason} ->
+      {error, Reason}
+  end.
