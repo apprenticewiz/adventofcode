f99caaae2ca64ee0c2fa80da326c91d281a9fd92
Chris Parrott <chris@parrotts.net>
2025-09-18 16:11:41 -0700

    tighten scan_line/3 slightly

--- prolog/2015/day08b/main.pl
+++ prolog/2015/day08b/main.pl
@@ -32,11 +32,10 @@ read_lines(Stream, Accum, Result) :-
 scan_line([], Accum, Accum).
 scan_line([Ch | Rest], Accum, FinalLen) :-
     (  member(Ch, [92, 34])
-    -> NewAccum is Accum + 2,
-       scan_line(Rest, NewAccum, FinalLen)
-    ;  NewAccum is Accum + 1,
-       scan_line(Rest, NewAccum, FinalLen)
-    ).
+    -> NewAccum is Accum + 2
+    ;  NewAccum is Accum + 1
+    ),
+    scan_line(Rest, NewAccum, FinalLen).
 
 usage :-
     tell(user_error),
