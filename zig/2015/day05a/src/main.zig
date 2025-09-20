787634e97d49ed9167cfc522cd58a378c35df649
Chris Parrott <chris@parrotts.net>
2025-09-20 00:36:12 -0700

    reworked solution for zig 0.15

--- zig/2015/day05a/src/main.zig
+++ zig/2015/day05a/src/main.zig
@@ -32,19 +32,21 @@ fn prop3(line: []const u8) bool {
         std.mem.count(u8, line, "xy") == 0;
 }
 
-fn processFile(allocator: std.mem.Allocator, filename: []const u8) !u32 {
+fn processFile(filename: []const u8) !u32 {
     var file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
-    var reader = std.io.bufferedReader(file.reader());
-    var stream = reader.reader();
+    var read_buf: [131072]u8 = undefined;
+    var reader = file.reader(&read_buf);
+    var r_interface = &reader.interface;
 
     var result: u32 = 0;
-    var line_buf = std.ArrayList(u8).init(allocator);
-    defer line_buf.deinit();
 
-    while ( try stream.readUntilDelimiterOrEofAlloc(allocator, '\n', 256) ) |line| {
-        defer allocator.free(line);
+    while ( true ) {
+        const line = r_interface.takeDelimiterExclusive('\n') catch |err| switch(err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
         if ( prop1(line) and prop2(line) and prop3(line) ) {
             result += 1;
         }
@@ -54,8 +56,10 @@ fn processFile(allocator: std.mem.Allocator, filename: []const u8) !u32 {
 }
 
 pub fn main() !void {
+    var stdout_buffer: [1024]u8 = undefined;
     const allocator = std.heap.page_allocator;
-    const stdout = std.io.getStdOut().writer();
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    var stdout = &stdout_writer.interface;
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
@@ -65,10 +69,11 @@ pub fn main() !void {
 
     const filename = args[1];
 
-    const result = processFile(allocator, filename) catch |err| {
+    const result = processFile(filename) catch |err| {
         std.debug.print("error while processing file `{s}': {s}\n", .{filename, @errorName(err)});
         std.process.exit(1);
     };
 
     try stdout.print("result = {}\n", .{result});
+    try stdout.flush();
 }
--- zig/2015/day05b/build.zig
+++ zig/2015/day05b/build.zig
@@ -6,9 +6,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "day05b",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     b.installArtifact(exe);
