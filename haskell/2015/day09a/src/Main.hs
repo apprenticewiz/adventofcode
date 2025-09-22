eaeeabfeca7fb920b0d8bee0f62e136172437a23
Chris Parrott <chris@parrotts.net>
2025-09-22 00:03:18 -0700

    2015 day 9 in Haskell

---
+++ haskell/2015/day09a/src/Main.hs
@@ -0,0 +1,71 @@
+module Main ( main ) where
+
+import Data.Int (Int32)
+import Data.List (permutations)
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+type City = String
+type Distances = Map.Map (City, City) Int
+
+city :: Parser City
+city = many1 letter
+
+line :: Parser (City, City, Int)
+line = do
+       city1 <- city
+       spaces
+       _ <- string "to"
+       spaces
+       city2 <- city
+       spaces
+       _ <- string "="
+       spaces
+       distance <- many1 digit
+       return (city1, city2, read distance)
+
+file :: Parser [(City, City, Int)]
+file = line `endBy1` newline
+
+buildMap :: [(City, City, Int)] -> Distances
+buildMap edges = Map.fromList [((a, b), n) | (a, b, n) <- edges ] <>
+                 Map.fromList [((b, a), n) | (a, b, n) <- edges ]
+
+dist :: Distances -> City -> City -> Int
+dist d a b = d Map.! (a, b)
+
+pathLen :: Distances -> [City] -> Int
+pathLen d cs = sum [ dist d a b | (a, b) <- zip cs (tail cs) ]
+
+shortestRoute :: [City] -> Distances -> Int
+shortestRoute cities d = minimum [ pathLen d p | p <- permutations cities ]
+
+process :: String -> Int32
+process content =
+    let edges = case parse file "" content of
+                    Left err -> error (show err)
+                    Right xs -> xs
+        distances = buildMap edges
+        cities = Set.toList (Set.fromList [ c | (a, b, _) <- edges, c <- [a, b] ])
+    in fromIntegral $ shortestRoute cities distances
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
