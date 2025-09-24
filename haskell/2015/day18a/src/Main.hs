c18b34c3ec298673c9bd6dab38c30987053fb050
Chris Parrott <chris@parrotts.net>
2025-09-24 12:52:09 -0700

    2015 day 18 in Haskell

---
+++ haskell/2015/day18a/src/Main.hs
@@ -0,0 +1,72 @@
+module Main (main) where
+
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.Int (Int32)
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
+type Grid = UArray (Int, Int) Bool
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+parseInput :: String -> Grid
+parseInput content =
+    let contentLines = lines content
+        numRows = length contentLines
+        numCols = length (contentLines !! 0)
+        mapChar (row, col) = case (contentLines !! (row - 1)) !! (col -1) of
+                                 '#' -> True
+                                 '.' -> False
+                                 _   -> error ("unexpected char in input at (" ++ show row ++ ", " ++ show col ++ ")")
+    in array ((1, 1), (numRows, numCols)) [((i, j), mapChar (i, j)) | i <- [1..numRows], j <- [1..numCols]]
+
+calcLight :: Grid -> (Int, Int) -> Bool
+calcLight grid (row, col) =
+    let ((rMin, cMin), (rMax, cMax)) = bounds grid
+        onCount = length [() | (dr, dc) <- [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)],
+                               let r = row + dr,
+                               let c = col + dc,
+                               r >= rMin, r <= rMax,
+                               c >= cMin, c <= cMax,
+                               grid ! (r, c)]
+        lightOn = grid ! (row, col)
+    in if lightOn then onCount >= 2 && onCount <= 3
+                  else onCount == 3
+
+step :: Grid -> Grid
+step grid = runSTUArray $ do
+                let gridBounds = bounds grid
+                mgrid <- newArray gridBounds False :: ST s (STUArray s (Int, Int) Bool)
+                forM_ (indices grid) $ \i -> do
+                    let newVal = calcLight grid i
+                    writeArray mgrid i newVal
+                return mgrid
+
+runSteps :: Int -> Grid -> Grid
+runSteps n grid = iterate step grid !! n
+
+countLights :: Grid -> Int
+countLights grid = length $ filter id (elems grid)
+
+process :: String -> Int32
+process content =
+    let grid = parseInput content
+    in fromIntegral $ countLights $ runSteps 100 grid
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
