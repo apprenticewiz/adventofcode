4926b767a5e680fe74ee1e8d22eea38a0915ca9e
Chris Parrott <chris@parrotts.net>
2025-10-07 13:46:46 -0700

    fix semantic issue in code: initial set of sues was fixed to 1..500, which works for input but not other possible inputs

--- haskell/2015/day16b/src/Main.hs
+++ haskell/2015/day16b/src/Main.hs
@@ -71,7 +71,7 @@ process content =
         Right sues -> fromIntegral $ Set.elemAt 0 $
             foldl'
                 (\acc (thing, amount) -> acc `Set.intersection` (possibleSet sues thing amount))
-                (Set.fromList [1..500])
+                (Set.fromList (map fst sues))
                 [("children", 3), ("cats", 7), ("samoyeds", 2), ("pomeranians", 3), ("akitas", 0),
                  ("vizslas", 0), ("goldfish", 5), ("trees", 3), ("cars", 2), ("perfumes", 1)]
 
