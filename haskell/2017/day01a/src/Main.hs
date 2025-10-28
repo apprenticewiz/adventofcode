5c6ff78479d07b1a82fe7b4ef443ee0e80c164ea
Chris Parrott <chris@parrotts.net>
2025-10-28 15:09:46 -0700

    2017 day 1 in Haskell

---
+++ haskell/2017/day01a/src/Main.hs
@@ -0,0 +1,27 @@
+module Main ( main ) where
+
+import System.Environment
+import System.Exit
+import System.IO
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+process :: String -> Int
+process content =
+    let l = head $ lines content
+        pairs = zip l (tail $ cycle l)
+    in sum [ read [x] | (x, y) <- pairs, x == y ]
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
