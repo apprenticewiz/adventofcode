53af4ba9c83292367e10ed48707d12ad21cac2be
Chris Parrott <chris@parrotts.net>
2025-10-29 00:01:50 -0700

    2017 day 4 in Haskell

---
+++ haskell/2017/day04b/src/Main.hs
@@ -0,0 +1,34 @@
+module Main ( main ) where
+
+import Data.List
+import Data.Maybe
+import System.Environment
+import System.Exit
+import System.IO
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+process :: String -> Int
+process content = (length . mapMaybe valid) (lines content)
+    where
+        valid :: String -> Maybe String
+        valid line =
+            let parts = words line
+                sortedParts = map sort parts
+            in if length sortedParts == length (nub sortedParts)
+                then Just line
+                else Nothing
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
