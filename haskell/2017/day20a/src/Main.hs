f520cf61968726e34bdca308097103979b27e3d6
Chris Parrott <chris@parrotts.net>
2025-11-06 02:09:56 -0800

    2017 day 20 in Haskell

---
+++ haskell/2017/day20a/src/Main.hs
@@ -0,0 +1,67 @@
+module Main ( main ) where
+
+import System.Environment
+import System.Exit
+import System.IO
+import Text.Parsec
+import Text.Parsec.String
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+file :: Parser [((Int, Int, Int), (Int, Int, Int), (Int, Int, Int))]
+file = line `sepEndBy1` newline <* eof
+
+line :: Parser ((Int, Int, Int), (Int, Int, Int), (Int, Int, Int))
+line = do
+    p <- parseTriple "p"
+    _ <- string ", "
+    v <- parseTriple "v"
+    _ <- string ", "
+    a <- parseTriple "a"
+    return (p, v, a)
+
+parseTriple :: String -> Parser (Int, Int, Int)
+parseTriple x = do
+    _ <- string (x ++ "=<")
+    n1 <- parseInt
+    _ <- string ","
+    n2 <- parseInt
+    _ <- string ","
+    n3 <- parseInt
+    _ <- string ">"
+    return (n1, n2, n3)
+
+parseInt :: Parser Int
+parseInt = read <$> ((++) <$> option "" (string "-") <*> many1 digit)
+
+manhattan :: (Int, Int, Int) -> Int
+manhattan (x, y, z) = abs x + abs y + abs z
+
+process :: String -> Int
+process content =
+    case parse file "" content of
+        Left err -> error (show err)
+        Right particles ->
+            let (_, _, _, closest, _) = foldl' step (maxBound, maxBound, maxBound, -1, 0) particles
+            in closest
+  where
+    step (minP, minV, minA, closest, n) particle =
+        let (p, v, a) = particle
+            (distP, distV, distA) = (manhattan p, manhattan v, manhattan a)
+        in if distA < minA || (distA == minA && distV < minV) || (distA == minA && distV == minV && distP < minP )
+            then (distP, distV, distA, n, n + 1)
+            else (minP, minV, minA, closest, n + 1)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
---
+++ haskell/2017/day20b/src/Main.hs
@@ -0,0 +1,74 @@
+module Main ( main ) where
+
+import qualified Data.Map.Strict as Map
+import System.Environment
+import System.Exit
+import System.IO
+import Text.Parsec
+import Text.Parsec.String
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+file :: Parser [((Int, Int, Int), (Int, Int, Int), (Int, Int, Int))]
+file = line `sepEndBy1` newline <* eof
+
+line :: Parser ((Int, Int, Int), (Int, Int, Int), (Int, Int, Int))
+line = do
+    p <- parseTriple "p"
+    _ <- string ", "
+    v <- parseTriple "v"
+    _ <- string ", "
+    a <- parseTriple "a"
+    return (p, v, a)
+
+parseTriple :: String -> Parser (Int, Int, Int)
+parseTriple x = do
+    _ <- string (x ++ "=<")
+    n1 <- parseInt
+    _ <- string ","
+    n2 <- parseInt
+    _ <- string ","
+    n3 <- parseInt
+    _ <- string ">"
+    return (n1, n2, n3)
+
+parseInt :: Parser Int
+parseInt = read <$> ((++) <$> option "" (string "-") <*> many1 digit)
+
+dot :: (Int, Int, Int) -> (Int, Int, Int) -> Int
+dot (x1, y1, z1) (x2, y2, z2) = (x1 * x2) + (y1 * y2) + (z1 * z2)
+
+process :: String -> Int
+process content =
+    case parse file "" content of
+        Left err -> error (show err)
+        Right particles ->
+            let finalParticles = loop particles
+            in length finalParticles
+  where
+    loop particles =
+        let particles' = map (\(p, (vx, vy, vz), (ax, ay, az)) -> (p, (vx + ax, vy + ay, vz + az), (ax, ay, az))) particles
+            particles'' = map (\((px, py, pz), (vx, vy, vz), a) -> ((px + vx, py + vy, pz + vz), (vx, vy, vz), a)) particles'
+            positions = foldl' (\ps (n, ((px, py, pz), _, _)) -> Map.insertWith (++) (px, py, pz) [n] ps)
+                Map.empty (zip [0..] particles'')
+            collisions = Map.filter (\x -> length x > 1) positions
+            collidedParticles = concat (Map.elems collisions)
+            particles''' = map snd $ filter (\(n, _) -> n `notElem` collidedParticles) (zip [0..] particles'')
+            dots = map (\(p, v, _) -> dot p v) particles'''
+        in if all (> 0) dots
+            then particles'''
+            else loop particles'''
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
