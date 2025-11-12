db76dc2c1647e9e678bd38b84d3ccdd3724034da
Chris Parrott <chris@parrotts.net>
2025-11-12 10:38:07 -0800

    2018 day 8 in Haskell

---
+++ haskell/2018/day08b/src/Main.hs
@@ -0,0 +1,52 @@
+module Main ( main ) where
+
+import System.Environment
+import System.Exit
+import System.IO
+
+data Node = Node { children :: [Node], metadata :: [Int] }
+            deriving (Show)
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+parseNode :: [Int] -> (Node, [Int])
+parseNode (numChildren:numMetadata:rest) =
+    let (kids, rest') = parseChildren numChildren rest
+        (md, rest'') = splitAt numMetadata rest'
+    in (Node kids md, rest'')
+parseNode _ = error "expected more data"
+
+parseChildren :: Int -> [Int] -> ([Node], [Int])
+parseChildren 0 xs = ([], xs)
+parseChildren n xs =
+    let (child, rest) = parseNode xs
+        (others, rest') = parseChildren (n - 1) rest
+    in (child : others, rest')
+
+computeValue :: Node -> Int
+computeValue (Node kids md) =
+    case kids of
+        [] -> sum md
+        _ ->
+            let selectedKids = [ kids !! (n - 1) | n <- md, n <= length kids ]
+            in sum $ map computeValue selectedKids
+
+process :: String -> Int
+process content =
+    let input = map read (words content) :: [Int]
+        (root, _) = parseNode input
+    in computeValue root
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let result = process content
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
