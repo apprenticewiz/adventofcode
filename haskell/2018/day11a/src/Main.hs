3373ed2ea6a706053dbb877acac212e8d119fc94
Chris Parrott <chris@parrotts.net>
2025-11-13 19:05:52 -0800

    2018 day 11 in Haskell

---
+++ haskell/2018/day11a/src/Main.hs
@@ -0,0 +1,50 @@
+module Main ( main ) where
+
+import Data.Array.Unboxed ( UArray )
+import qualified Data.Array.Unboxed as Array
+import System.Environment
+import System.Exit
+import System.IO
+
+gridDimensions :: (Int, Int)
+gridDimensions = (300, 300)
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <serial>"
+    exitFailure
+
+process :: Int -> (Int, Int)
+process serial =
+    let (maxX, maxY) = gridDimensions
+        grid = Array.array ((1, 1), gridDimensions) [ ((x, y), n) | x <- [1..maxX], y <- [1..maxY], let n = calcPower (x, y) ]
+    in findBestPower grid minBound (0, 0) [ (x, y) | x <- [1..maxX - 2], y <- [1..maxY - 2] ]
+  where
+    calcPower :: (Int, Int) -> Int
+    calcPower (x, y) =
+        let rackId = x + 10
+            power = ((rackId * y) + serial) * rackId
+            hundreds = (power `div` 100) `mod` 10
+        in hundreds - 5
+
+    findBestPower :: UArray (Int, Int) Int -> Int -> (Int, Int) -> [(Int, Int)] -> (Int, Int)
+    findBestPower grid bestPower bestPos coords
+      | null coords = bestPos
+      | otherwise =
+            let (currPos:rest) = coords
+                (x, y) = currPos
+                squarePower = sum [ grid Array.! (i, j) | i <- [x..x+2], j <- [y..y+2] ]
+            in if squarePower > bestPower
+                then findBestPower grid squarePower currPos rest
+                else findBestPower grid bestPower bestPos rest
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [input] -> do
+            let serial = read input
+                result = process serial
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
---
+++ haskell/2018/day11b/src/Main.hs
@@ -0,0 +1,91 @@
+module Main ( main ) where
+
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed ( UArray )
+import qualified Data.Array.Unboxed as Array
+import Data.List
+import Data.Ord
+import System.Environment
+import System.Exit
+import System.IO
+
+gridDimensions :: (Int, Int)
+gridDimensions = (300, 300)
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <serial>"
+    exitFailure
+
+buildGrid :: Int -> UArray (Int, Int) Int
+buildGrid serial =
+    let (maxX, maxY) = gridDimensions
+    in Array.array ((1, 1),(maxX, maxY))
+         [ ((x, y), power serial x y)
+         | x <- [1..maxX], y <- [1..maxY] ]
+
+power :: Int -> Int -> Int -> Int
+power serial x y =
+    let rack = x + 10
+        p    = (rack * y + serial) * rack
+    in (p `div` 100 `mod` 10) - 5
+
+buildSAT :: UArray (Int,Int) Int -> UArray (Int,Int) Int
+buildSAT grid = runSTUArray $ do
+    let ((x1, y1),(x2, y2)) = Array.bounds grid
+    sat <- newArray ((x1, y1),(x2, y2)) 0 :: ST s (STUArray s (Int,Int) Int)
+    forM_ [x1..x2] $ \x ->
+        forM_ [y1..y2] $ \y -> do
+            let g = grid Array.! (x, y)
+            left  <- if x > x1 then readArray sat (x-1, y) else return 0
+            above <- if y > y1 then readArray sat (x, y-1) else return 0
+            diag  <- if x > x1 && y > y1 then readArray sat (x-1, y-1) else return 0
+            writeArray sat (x, y) (g + left + above - diag)
+    return sat
+
+squareSum :: UArray (Int, Int) Int -> Int -> Int -> Int -> Int
+squareSum sat x y s =
+      sat Array.! (x2, y2)
+    - satAt (x1, y2)
+    - satAt (x2 ,y1)
+    + satAt (x1, y1)
+  where
+    (x1, y1) = (x-1, y-1)
+    (x2, y2) = (x+s-1, y+s-1)
+    ((lx, ly), _) = Array.bounds sat
+
+    satAt (i,j)
+        | i < lx || j < ly = 0
+        | otherwise        = sat Array.! (i, j)
+
+bestForSize :: UArray (Int, Int) Int -> Int -> Int -> Int -> (Int, Int, Int, Int)
+bestForSize sat s maxX maxY =
+    maximumBy (comparing (\(_ ,_ ,_ , p)->p))
+      [ (x,y,s, squareSum sat x y s)
+      | x <- [1 .. maxX - s + 1]
+      , y <- [1 .. maxY - s + 1]
+      ]
+
+process :: Int -> (Int, Int, Int)
+process serial =
+    let (maxX, maxY) = gridDimensions
+        grid = buildGrid serial
+        sat  = buildSAT grid
+        candidates =
+            [ bestForSize sat n maxX maxY
+            | n <- [1..maxX] ]
+        (x, y, s, _) = maximumBy (comparing (\(_ ,_ ,_ ,p) -> p)) candidates
+    in (x, y, s)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [input] -> do
+            let serial = read input
+                result = process serial
+            putStrLn $ "result = " ++ show result
+        _ -> usage progname
