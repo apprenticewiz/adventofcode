dbd6edc5557dbe5962837c88dcd932ad4508eef1
Chris Parrott <chris@parrotts.net>
2025-12-04 16:11:51 -0800

    add timing boilerplate to code

--- haskell/2025/day01a/src/Main.hs
+++ haskell/2025/day01a/src/Main.hs
@@ -1,5 +1,7 @@
 module Main ( main ) where
 
+import Control.DeepSeq
+import System.Clock
 import System.Environment
 import System.Exit
 import System.IO
@@ -26,13 +28,29 @@ usage progname = do
     hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
     exitFailure
 
+showTime :: TimeSpec -> String
+showTime elapsed =
+    let ns = fromIntegral (toNanoSecs elapsed) :: Double
+    in if ns < 1000
+       then show ns ++ " ns"
+       else if ns < 1000000
+       then show (ns / 1000.0) ++ " μs"
+       else if ns < 10000000000
+            then show (ns / 1000000.0) ++ " ms"
+            else show (ns / 1000000000.0) ++ " s"
+
 main :: IO ()
 main = do
     args <- getArgs
     progname <- getProgName
     case args of
         [filename] -> do
+            start <- getTime Monotonic
             content <- readFile filename
             let result = process content
+            result `deepseq` return ()
+            end <- getTime Monotonic
+            let elapsed = diffTimeSpec start end
             putStrLn $ "result = " ++ show result
+            putStrLn $ "elapsed time: " ++ showTime elapsed
         _ -> usage progname
