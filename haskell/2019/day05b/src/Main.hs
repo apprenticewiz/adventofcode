52d9594fa2dc2f2e2f0230657276ee646e9b9b4b
Chris Parrott <chris@parrotts.net>
2026-02-04 16:06:18 -0800

    2019 day 5 in Haskell

---
+++ haskell/2019/day05b/src/Main.hs
@@ -0,0 +1,50 @@
+module Main ( main ) where
+
+import Control.DeepSeq
+import System.Clock
+import System.Environment
+import System.Exit
+import System.IO
+
+import IntCode
+
+process :: String -> Int
+process content =
+    let initMem = map read (words (map (\c -> if c == ',' then ' ' else c) content))
+        cpu0 = initCpu initMem 5
+        outputs = execute cpu0
+    in case outputs of
+        output:_ -> output
+        [] -> error "no outputs"
+
+usage :: String -> IO ()
+usage progname = do
+    hPutStrLn stderr $ "usage: " ++ progname ++ " <input file>"
+    exitFailure
+
+showTime :: TimeSpec -> String
+showTime elapsed =
+    let ns = fromIntegral (toNanoSecs elapsed) :: Double
+    in if ns < 1000
+       then show ns ++ " ns"
+       else if ns < 1000000
+       then show (ns / 1000.0) ++ " μs"
+       else if ns < 1000000000
+            then show (ns / 1000000.0) ++ " ms"
+            else show (ns / 1000000000.0) ++ " s"
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progname <- getProgName
+    case args of
+        [filename] -> do
+            start <- getTime Monotonic
+            content <- readFile filename
+            let result = process content
+            result `deepseq` return ()
+            end <- getTime Monotonic
+            let elapsed = diffTimeSpec start end
+            putStrLn $ "result = " ++ show result
+            putStrLn $ "elapsed time: " ++ showTime elapsed
+        _ -> usage progname
